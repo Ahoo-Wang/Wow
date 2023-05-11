b4608b8e41d50a43d00bb0e7a1d51e994a8513e7
Ahoo Wang <ahoowang@qq.com>
2023-05-11 11:29:33 +0800

    Improve code coverage

---
+++ wow-core/src/test/kotlin/me/ahoo/wow/command/LocalFirstCommandBusTest.kt
@@ -0,0 +1,10 @@
+package me.ahoo.wow.command
+
+import me.ahoo.wow.tck.command.CommandBusSpec
+
+class LocalFirstCommandBusTest : CommandBusSpec() {
+    override fun createCommandBus(): CommandBus {
+        return LocalFirstCommandBus(distributedCommandBus = InMemoryCommandBus())
+    }
+
+}
