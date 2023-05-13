acdba959f4bb73c563cfd256a946831548a571bd
Ahoo Wang <ahoowang@qq.com>
2023-05-13 13:28:24 +0800

    feat: support TracingLocalMessageBus

---
+++ wow-opentelemetry/src/test/kotlin/me/ahoo/wow/opentelemetry/messaging/TracingLocalEventBusTest.kt
@@ -0,0 +1,33 @@
+package me.ahoo.wow.opentelemetry.messaging
+
+import io.mockk.every
+import io.mockk.mockk
+import me.ahoo.wow.api.event.DomainEvent
+import me.ahoo.wow.event.DomainEventStream
+import me.ahoo.wow.event.InMemoryDomainEventBus
+import me.ahoo.wow.id.GlobalIdGenerator
+import me.ahoo.wow.modeling.asAggregateId
+import me.ahoo.wow.opentelemetry.MonoTraceTest
+import me.ahoo.wow.opentelemetry.messaging.Tracing.tracing
+import org.junit.jupiter.api.Test
+import reactor.kotlin.test.test
+
+class TracingLocalEventBusTest {
+
+    @Test
+    fun send() {
+        val event = mockk<DomainEvent<*>> {
+            every { name } returns "test"
+        }
+        val eventStream = mockk<DomainEventStream> {
+            every { id } returns GlobalIdGenerator.generateAsString()
+            every { aggregateName } returns MonoTraceTest.TEST_NAMED_AGGREGATE.aggregateName
+            every { aggregateId } returns MonoTraceTest.TEST_NAMED_AGGREGATE.asAggregateId()
+            every { iterator() } returns listOf(event).iterator()
+        }
+        InMemoryDomainEventBus().tracing()
+            .send(eventStream)
+            .test()
+            .verifyComplete()
+    }
+}
