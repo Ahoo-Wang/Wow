f34863f8ba6218bfc70e5c09adf6ca0f142888fb
Ahoo Wang <ahoowang@qq.com>
2023-05-16 22:52:24 +0800

    feat: add `AutoCloseable` to `KafkaBus`

--- wow-kafka/src/main/kotlin/me/ahoo/wow/kafka/AbstractKafkaBus.kt
+++ wow-kafka/src/main/kotlin/me/ahoo/wow/kafka/AbstractKafkaBus.kt
@@ -39,7 +39,7 @@ abstract class AbstractKafkaBus<M, E>(
     private val sender: KafkaSender<String, String>,
     private val receiverOptions: ReceiverOptions<String, String>,
     private val receiverOptionsCustomizer: ReceiverOptionsCustomizer = NoOpReceiverOptionsCustomizer
-) : MessageBus where M : Message<*>, M : AggregateIdCapable, M : NamedAggregate, E : MessageExchange<*> {
+) : MessageBus, AutoCloseable where M : Message<*>, M : AggregateIdCapable, M : NamedAggregate, E : MessageExchange<*> {
     companion object {
         private val log = LoggerFactory.getLogger(AbstractKafkaBus::class.java)
     }
@@ -110,5 +110,9 @@ abstract class AbstractKafkaBus<M, E>(
             }
             null
         }
+    }
+
+    override fun close() {
+        sender.close()
     }
 }
