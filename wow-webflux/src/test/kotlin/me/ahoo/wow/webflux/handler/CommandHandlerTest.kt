b9c4ecc5316184bcb234b8e3cba1990a627131e4
Ahoo Wang <ahoowang@qq.com>
2023-06-13 23:38:36 +0800

    Improve code coverage

--- wow-webflux/src/test/kotlin/me/ahoo/wow/webflux/handler/CommandHandlerTest.kt
+++ wow-webflux/src/test/kotlin/me/ahoo/wow/webflux/handler/CommandHandlerTest.kt
@@ -13,11 +13,12 @@ import org.springframework.web.reactive.function.server.ServerRequest
 import reactor.kotlin.core.publisher.toMono
 import reactor.kotlin.test.test
 import java.security.Principal
+import java.time.Duration
 
 class CommandHandlerTest {
 
     @Test
-    fun handle() {
+    fun handleSent() {
         val request = mockk<ServerRequest> {
             every { headers().firstHeader(CommandHeaders.WAIT_STAGE) } returns "SENT"
             every { headers().firstHeader(CommandHeaders.WAIT_CONTEXT) } returns "test"
@@ -37,5 +38,27 @@ class CommandHandlerTest {
         ).test()
             .expectNextCount(1)
             .verifyComplete()
+    }
+
+    @Test
+    fun handleProcessed() {
+        val request = mockk<ServerRequest> {
+            every { headers().firstHeader(CommandHeaders.WAIT_STAGE) } returns "PROCESSED"
+            every { headers().firstHeader(CommandHeaders.WAIT_CONTEXT) } returns "test"
+            every { headers().firstHeader(CommandHeaders.WAIT_PROCESSOR) } returns "test"
+            every { headers().firstHeader(CommandHeaders.WAIT_TIME_OUT) } returns 10.toString()
+            every { pathVariables()[MessageRecords.TENANT_ID] } returns GlobalIdGenerator.generateAsString()
+            every { headers().firstHeader(CommandHeaders.AGGREGATE_VERSION) } returns null
+            every { headers().firstHeader(CommandHeaders.REQUEST_ID) } returns null
+            every { principal() } returns mockk<Principal> {
+                every { name } returns GlobalIdGenerator.generateAsString()
+            }.toMono()
+        }
+        val commandHandler = CommandHandler(MOCK_AGGREGATE_METADATA, SagaVerifier.defaultCommandGateway())
+        commandHandler.handle(
+            request,
+            MockCreateAggregate(GlobalIdGenerator.generateAsString(), GlobalIdGenerator.generateAsString())
+        ).test()
+            .verifyTimeout(Duration.ofMillis(110))
     }
 }
