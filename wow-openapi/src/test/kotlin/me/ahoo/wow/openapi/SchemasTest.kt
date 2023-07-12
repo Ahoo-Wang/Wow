09a930ccf0fa60890f18dedf99986f1e56e9d0a2
Ahoo Wang <ahoowang@qq.com>
2023-07-12 23:57:59 +0800

    update version

--- wow-openapi/src/test/kotlin/me/ahoo/wow/openapi/SchemasTest.kt
+++ wow-openapi/src/test/kotlin/me/ahoo/wow/openapi/SchemasTest.kt
@@ -4,19 +4,31 @@ import me.ahoo.wow.openapi.Schemas.asSchemas
 import me.ahoo.wow.openapi.Schemas.getSchemaRef
 import me.ahoo.wow.openapi.route.MockCommandRoute
 import me.ahoo.wow.tck.mock.MockStateAggregate
-import org.hamcrest.CoreMatchers
+import org.hamcrest.CoreMatchers.anything
+import org.hamcrest.CoreMatchers.equalTo
+import org.hamcrest.CoreMatchers.notNullValue
+import org.hamcrest.CoreMatchers.nullValue
 import org.hamcrest.MatcherAssert.*
 import org.junit.jupiter.api.Test
 
 class SchemasTest {
-
     @Test
     fun asSchemas() {
+        val schemas = buildList<Class<*>?> {
+            add(MockStateAggregate::class.java)
+        }.asSchemas()
+        assertThat(schemas, anything())
+        assertThat(schemas.values.first().properties, notNullValue())
+    }
+
+    @Test
+    fun asSchemasWithVariable() {
         val schemas = buildList<Class<*>?> {
             add(MockCommandRoute::class.java)
             add(null)
         }.asSchemas()
-        assertThat(schemas, CoreMatchers.anything())
+        assertThat(schemas, anything())
+        assertThat(schemas.values.first().properties, nullValue())
     }
 
     @Test
@@ -24,7 +36,7 @@ class SchemasTest {
         val schemaRef = MockStateAggregate::class.java.getSchemaRef()
         assertThat(
             schemaRef.`$ref`,
-            CoreMatchers.equalTo("#/components/schemas/${MockStateAggregate::class.java.simpleName}")
+            equalTo("#/components/schemas/${MockStateAggregate::class.java.simpleName}")
         )
     }
 }
