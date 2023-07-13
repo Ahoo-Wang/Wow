5cfdbe8c3479a3171816fbedb61e9e27eed045c3
Ahoo Wang <ahoowang@qq.com>
2023-07-13 15:07:33 +0800

    feat: support `ScanAggregateHandlerFunction`

--- wow-webflux/src/test/kotlin/me/ahoo/wow/webflux/route/query/AggregateTracingHandlerFunctionTest.kt
+++ wow-webflux/src/test/kotlin/me/ahoo/wow/webflux/route/query/AggregateTracingHandlerFunctionTest.kt
@@ -1,4 +1,17 @@
-package me.ahoo.wow.webflux.route
+/*
+ * Copyright [2021-present] [ahoo wang <ahoowang@qq.com> (https://github.com/Ahoo-Wang)].
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package me.ahoo.wow.webflux.route.query
 
 import io.mockk.every
 import io.mockk.mockk
@@ -8,7 +21,6 @@ import me.ahoo.wow.openapi.RoutePaths
 import me.ahoo.wow.tck.mock.MOCK_AGGREGATE_METADATA
 import me.ahoo.wow.webflux.exception.DefaultExceptionHandler
 import me.ahoo.wow.webflux.route.command.CommandParser.getTenantId
-import me.ahoo.wow.webflux.route.query.AggregateTracingHandlerFunction
 import org.hamcrest.MatcherAssert.*
 import org.hamcrest.Matchers.*
 import org.junit.jupiter.api.Test
--- wow-webflux/src/test/kotlin/me/ahoo/wow/webflux/route/query/LoadAggregateHandlerFunctionTest.kt
+++ wow-webflux/src/test/kotlin/me/ahoo/wow/webflux/route/query/LoadAggregateHandlerFunctionTest.kt
@@ -1,7 +1,21 @@
-package me.ahoo.wow.webflux.route
+/*
+ * Copyright [2021-present] [ahoo wang <ahoowang@qq.com> (https://github.com/Ahoo-Wang)].
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package me.ahoo.wow.webflux.route.query
 
 import io.mockk.every
 import io.mockk.mockk
+import me.ahoo.wow.eventsourcing.EventSourcingStateAggregateRepository
 import me.ahoo.wow.eventsourcing.InMemoryEventStore
 import me.ahoo.wow.eventsourcing.snapshot.NoOpSnapshotRepository
 import me.ahoo.wow.id.GlobalIdGenerator
@@ -10,7 +24,6 @@ import me.ahoo.wow.openapi.RoutePaths
 import me.ahoo.wow.tck.mock.MOCK_AGGREGATE_METADATA
 import me.ahoo.wow.webflux.exception.DefaultExceptionHandler
 import me.ahoo.wow.webflux.route.command.CommandParser.getTenantId
-import me.ahoo.wow.webflux.route.snapshot.RegenerateSnapshotHandlerFunction
 import org.hamcrest.MatcherAssert.*
 import org.hamcrest.Matchers.*
 import org.junit.jupiter.api.Test
@@ -18,16 +31,18 @@ import org.springframework.http.HttpStatus
 import org.springframework.web.reactive.function.server.ServerRequest
 import reactor.kotlin.test.test
 
-class RegenerateSnapshotHandlerFunctionTest {
+class LoadAggregateHandlerFunctionTest {
 
     @Test
     fun handle() {
-        val handlerFunction = RegenerateSnapshotHandlerFunction(
-            MOCK_AGGREGATE_METADATA,
-            ConstructorStateAggregateFactory,
-            InMemoryEventStore(),
-            NoOpSnapshotRepository,
-            DefaultExceptionHandler,
+        val handlerFunction = LoadAggregateHandlerFunction(
+            aggregateMetadata = MOCK_AGGREGATE_METADATA,
+            stateAggregateRepository = EventSourcingStateAggregateRepository(
+                stateAggregateFactory = ConstructorStateAggregateFactory,
+                snapshotRepository = NoOpSnapshotRepository,
+                eventStore = InMemoryEventStore(),
+            ),
+            exceptionHandler = DefaultExceptionHandler,
         )
         val request = mockk<ServerRequest> {
             every { pathVariable(RoutePaths.ID_KEY) } returns GlobalIdGenerator.generateAsString()
