e51cff78e8190a7d7089c3987d3fa8ace39aa2a9
Ahoo Wang <ahoowang@qq.com>
2023-12-07 20:36:49 +0800

    Improve code coverage

---
+++ wow-webflux/src/test/kotlin/me/ahoo/wow/webflux/wait/CommandWaitHandlerFunctionTest.kt
@@ -0,0 +1,35 @@
+package me.ahoo.wow.webflux.wait
+
+import io.mockk.every
+import io.mockk.mockk
+import me.ahoo.wow.command.wait.CommandStage
+import me.ahoo.wow.command.wait.SimpleWaitSignal
+import me.ahoo.wow.command.wait.SimpleWaitStrategyRegistrar
+import org.hamcrest.MatcherAssert.*
+import org.hamcrest.Matchers.*
+import org.junit.jupiter.api.Test
+import org.springframework.web.reactive.function.server.ServerRequest
+import reactor.kotlin.core.publisher.toMono
+import reactor.kotlin.test.test
+
+class CommandWaitHandlerFunctionTest {
+
+    @Test
+    fun handle() {
+        val commandWaitHandlerFunction = CommandWaitHandlerFunction(SimpleWaitStrategyRegistrar)
+        val request = mockk<ServerRequest> {
+            every { bodyToMono(SimpleWaitSignal::class.java) } returns SimpleWaitSignal(
+                commandId = "commandId",
+                stage = CommandStage.SENT,
+                contextName = "contextName",
+                processorName = "processorName",
+            ).toMono()
+        }
+        val response = commandWaitHandlerFunction.handle(request)
+        response.test()
+            .consumeNextWith {
+                assertThat(it.statusCode().is2xxSuccessful, equalTo(true))
+            }
+            .verifyComplete()
+    }
+}
