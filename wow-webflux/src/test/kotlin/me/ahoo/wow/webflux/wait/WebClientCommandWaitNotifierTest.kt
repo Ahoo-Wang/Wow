ad2810af3511e8bb72111b65e11880a0d5a2df60
Ahoo Wang <ahoowang@qq.com>
2023-12-07 20:47:48 +0800

    Improve code coverage (#315)

---
+++ wow-webflux/src/test/kotlin/me/ahoo/wow/webflux/wait/WebClientCommandWaitNotifierTest.kt
@@ -0,0 +1,58 @@
+package me.ahoo.wow.webflux.wait
+
+import io.mockk.every
+import io.mockk.mockk
+import me.ahoo.wow.command.wait.CommandStage
+import me.ahoo.wow.command.wait.SimpleWaitSignal
+import me.ahoo.wow.command.wait.SimpleWaitStrategyRegistrar
+import me.ahoo.wow.id.GlobalIdGenerator
+import org.junit.jupiter.api.Test
+import org.springframework.web.reactive.function.client.WebClient
+import reactor.core.publisher.Mono
+import reactor.kotlin.test.test
+
+class WebClientCommandWaitNotifierTest {
+
+    @Test
+    fun notifyLocal() {
+        val webClient = mockk<WebClient>()
+        val commandWaitNotifier = WebClientCommandWaitNotifier(SimpleWaitStrategyRegistrar, webClient)
+        val commandWaitEndpoint = "http://localhost:8080/command/wait"
+        val waitSignal = SimpleWaitSignal(
+            commandId = GlobalIdGenerator.generateAsString(),
+            stage = CommandStage.SENT,
+            contextName = "contextName",
+            processorName = "processorName",
+        )
+        commandWaitNotifier.notify(commandWaitEndpoint, waitSignal)
+            .test()
+            .verifyComplete()
+    }
+
+    @Test
+    fun notifyRemote() {
+        val commandWaitEndpoint = "http://localhost:8080/command/wait"
+        val webClient = mockk<WebClient> {
+            every {
+                post()
+                    .uri(commandWaitEndpoint)
+                    .contentType(any())
+                    .bodyValue(any())
+                    .retrieve()
+                    .bodyToMono(Void::class.java)
+                    .retryWhen(any())
+            } returns Mono.empty()
+        }
+        val commandWaitNotifier = WebClientCommandWaitNotifier(SimpleWaitStrategyRegistrar, webClient)
+
+        val waitSignal = SimpleWaitSignal(
+            commandId = "0ToC0Bez003X00Z",
+            stage = CommandStage.SENT,
+            contextName = "contextName",
+            processorName = "processorName",
+        )
+        commandWaitNotifier.notify(commandWaitEndpoint, waitSignal)
+            .test()
+            .verifyComplete()
+    }
+}
