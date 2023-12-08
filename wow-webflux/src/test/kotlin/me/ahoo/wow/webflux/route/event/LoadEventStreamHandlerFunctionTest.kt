6678e1c4da29e1869d1aedc0ba06d42daf7ae5e7
Ahoo Wang <ahoowang@qq.com>
2023-12-08 12:11:15 +0800

    Improve code coverage

---
+++ wow-webflux/src/test/kotlin/me/ahoo/wow/webflux/route/event/LoadEventStreamHandlerFunctionTest.kt
@@ -0,0 +1,38 @@
+package me.ahoo.wow.webflux.route.event
+
+import io.mockk.every
+import io.mockk.mockk
+import me.ahoo.wow.eventsourcing.InMemoryEventStore
+import me.ahoo.wow.id.GlobalIdGenerator
+import me.ahoo.wow.openapi.RoutePaths
+import me.ahoo.wow.tck.mock.MOCK_AGGREGATE_METADATA
+import me.ahoo.wow.webflux.route.command.CommandParser.getTenantId
+import org.hamcrest.MatcherAssert
+import org.hamcrest.Matchers
+import org.junit.jupiter.api.Test
+import org.springframework.http.HttpStatus
+import org.springframework.web.reactive.function.server.ServerRequest
+import reactor.kotlin.test.test
+
+class LoadEventStreamHandlerFunctionTest {
+
+    @Test
+    fun handle() {
+        val eventStore = InMemoryEventStore()
+        val handlerFunction = LoadEventStreamHandlerFunction(
+            aggregateMetadata = MOCK_AGGREGATE_METADATA,
+            eventStore = eventStore,
+        )
+        val request = mockk<ServerRequest> {
+            every { pathVariable(RoutePaths.ID_KEY) } returns GlobalIdGenerator.generateAsString()
+            every { pathVariable(RoutePaths.COMPENSATE_HEAD_VERSION_KEY) } returns "0"
+            every { pathVariable(RoutePaths.COMPENSATE_TAIL_VERSION_KEY) } returns Int.MAX_VALUE.toString()
+            every { getTenantId(aggregateMetadata = MOCK_AGGREGATE_METADATA) } returns GlobalIdGenerator.generateAsString()
+        }
+        handlerFunction.handle(request)
+            .test()
+            .consumeNextWith {
+                MatcherAssert.assertThat(it.statusCode(), Matchers.equalTo(HttpStatus.OK))
+            }.verifyComplete()
+    }
+}
