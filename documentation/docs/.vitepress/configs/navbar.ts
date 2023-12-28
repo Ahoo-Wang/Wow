ccdcf7ffca16f7b52fb7042d59fde7636975aa42
Ahoo Wang <ahoowang@qq.com>
2023-12-28 20:40:40 +0800

    feat(doc): add Saga distributed transactions pattern

--- documentation/docs/.vitepress/configs/navbar.ts
+++ documentation/docs/.vitepress/configs/navbar.ts
@@ -23,46 +23,63 @@ export const navbar: DefaultTheme.NavItem[] = [
         text: "资源",
         items: [
             {
-                text: 'CosId - 通用、灵活、高性能的分布式 ID 生成器',
-                link: 'https://github.com/Ahoo-Wang/CosId'
-            },
-            {
-                text: 'CoSky - 高性能、低成本微服务治理平台',
-                link: 'https://github.com/Ahoo-Wang/CoSky'
-            },
-            {
-                text: 'CoSec - 基于 RBAC 和策略的多租户响应式安全框架',
-                link: 'https://github.com/Ahoo-Wang/CoSec'
-            },
-            {
-                text: 'CoCache - 分布式一致性二级缓存框架',
-                link: 'https://github.com/Ahoo-Wang/CoCache'
-            },
-            {
-                text: 'Simba - 易用、灵活的分布式锁服务',
-                link: 'https://github.com/Ahoo-Wang/Simba'
-            },
-            {
                 text: '用于快速构建基于 Wow 框架的 DDD 项目模板',
                 link: 'https://github.com/Ahoo-Wang/wow-project-template'
             },
             {
-                text: 'Event Sourcing - Specifications',
-                link: 'https://abdullin.com/post/event-sourcing-specifications/'
+                text: '开源项目 - 微服务治理',
+                items: [
+                    {
+                        text: 'CosId - 通用、灵活、高性能的分布式 ID 生成器',
+                        link: 'https://github.com/Ahoo-Wang/CosId'
+                    },
+                    {
+                        text: 'CoSky - 高性能、低成本微服务治理平台',
+                        link: 'https://github.com/Ahoo-Wang/CoSky'
+                    },
+                    {
+                        text: 'CoSec - 基于 RBAC 和策略的多租户响应式安全框架',
+                        link: 'https://github.com/Ahoo-Wang/CoSec'
+                    },
+                    {
+                        text: 'CoCache - 分布式一致性二级缓存框架',
+                        link: 'https://github.com/Ahoo-Wang/CoCache'
+                    },
+                    {
+                        text: 'Simba - 易用、灵活的分布式锁服务',
+                        link: 'https://github.com/Ahoo-Wang/Simba'
+                    }
+                ]
             },
             {
-                text: 'Testing Event Sourcing',
-                link: 'https://event-driven.io/en/testing_event_sourcing/'
+                text: 'CQRS',
+                items: [
+                    {
+                        text: 'Event Sourcing - Specifications',
+                        link: 'https://abdullin.com/post/event-sourcing-specifications/'
+                    },
+                    {
+                        text: 'Testing Event Sourcing',
+                        link: 'https://event-driven.io/en/testing_event_sourcing/'
+                    }, {
+                        text: 'CQRS Journey',
+                        link: 'https://learn.microsoft.com/en-us/previous-versions/msp-n-p/jj554200(v=pandp.10)'
+                    }, {
+                        text: 'Saga distributed transactions pattern',
+                        link: 'https://learn.microsoft.com/en-us/azure/architecture/reference-architectures/saga/saga'
+                    }
+                ]
             },
             {
-                text: '响应式宣言',
-                link: 'https://www.reactivemanifesto.org/zh-CN'
-            }, {
-                text: 'R2DBC',
-                link: 'https://r2dbc.io/'
-            }, {
-                text: 'CQRS Journey',
-                link: 'https://learn.microsoft.com/en-us/previous-versions/msp-n-p/jj554200(v=pandp.10)'
+                text: '响应式编程',
+                items: [
+                    {
+                        text: '响应式宣言',
+                        link: 'https://www.reactivemanifesto.org/zh-CN'
+                    }, {
+                        text: 'R2DBC',
+                        link: 'https://r2dbc.io/'
+                    }]
             }
         ]
     },
