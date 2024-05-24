71357c3986c46cb763b1a5a24df0ca907512be3d
Ahoo Wang <ahoowang@qq.com>
2024-05-24 10:53:18 +0800

    feat(model): Support for returning `code` as a result to `CommandResult`. (#762)

--- example/example-domain/src/main/kotlin/me/ahoo/wow/example/domain/order/Order.kt
+++ example/example-domain/src/main/kotlin/me/ahoo/wow/example/domain/order/Order.kt
@@ -17,7 +17,7 @@ package me.ahoo.wow.example.domain.order
 import me.ahoo.wow.api.annotation.AggregateRoot
 import me.ahoo.wow.api.annotation.Name
 import me.ahoo.wow.api.command.CommandMessage
-import me.ahoo.wow.command.CommandResultAccessor
+import me.ahoo.wow.api.command.CommandResultAccessor
 import me.ahoo.wow.command.ServerCommandExchange
 import me.ahoo.wow.event.DomainEventStream
 import me.ahoo.wow.example.api.order.AddressChanged
