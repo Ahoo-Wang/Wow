43695f15cc73b17f409a9d192c31d1bf46976751
Ahoo Wang <ahoowang@qq.com>
2024-06-07 17:25:58 +0800

    feat(query): Support `rewriteQuery` & `rewriteResult` for `SnapshotQueryContext`

--- wow-query/src/main/kotlin/me/ahoo/wow/query/filter/SnapshotQueryContext.kt
+++ wow-query/src/main/kotlin/me/ahoo/wow/query/filter/SnapshotQueryContext.kt
@@ -19,7 +19,6 @@ import me.ahoo.wow.api.query.IListQuery
 import me.ahoo.wow.api.query.IPagedQuery
 import me.ahoo.wow.api.query.ISingleQuery
 import me.ahoo.wow.api.query.PagedList
-import me.ahoo.wow.api.query.RewritableCondition
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 import java.util.concurrent.ConcurrentHashMap
@@ -28,7 +27,7 @@ const val QUERY_KEY = "__QUERY__"
 const val RESULT_KEY = "__RESULT__"
 
 @Suppress("UNCHECKED_CAST")
-interface SnapshotQueryContext<SOURCE : SnapshotQueryContext<SOURCE, Q, R>, Q : RewritableCondition<Q>, R : Any> {
+interface SnapshotQueryContext<SOURCE : SnapshotQueryContext<SOURCE, Q, R>, Q : Any, R : Any> {
     val attributes: MutableMap<String, Any>
     val namedAggregate: NamedAggregate
     val queryType: QueryType
