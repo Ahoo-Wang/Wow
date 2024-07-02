503115016e3b2066436b0ee5f01ee419f988e0cc
Ahoo Wang <ahoowang@qq.com>
2024-07-02 18:52:18 +0800

    feat(core): add `AggregateIdempotencyCheckerProvider` api

--- wow-spring-boot-starter/src/test/kotlin/me/ahoo/wow/spring/boot/starter/command/CommandGatewayAutoConfigurationTest.kt
+++ wow-spring-boot-starter/src/test/kotlin/me/ahoo/wow/spring/boot/starter/command/CommandGatewayAutoConfigurationTest.kt
@@ -5,13 +5,16 @@ import me.ahoo.cosid.machine.HostAddressSupplier
 import me.ahoo.cosid.machine.LocalHostAddressSupplier
 import me.ahoo.wow.command.CommandGateway
 import me.ahoo.wow.command.wait.CommandWaitNotifier
+import me.ahoo.wow.id.GlobalIdGenerator
 import me.ahoo.wow.infra.idempotency.AggregateIdempotencyCheckerProvider
 import me.ahoo.wow.spring.boot.starter.BusType
 import me.ahoo.wow.spring.boot.starter.enableWow
+import me.ahoo.wow.tck.mock.MOCK_AGGREGATE_METADATA
 import org.assertj.core.api.AssertionsForInterfaceTypes
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.assertj.AssertableApplicationContext
 import org.springframework.boot.test.context.runner.ApplicationContextRunner
+import reactor.kotlin.test.test
 
 class CommandGatewayAutoConfigurationTest {
     private val contextRunner = ApplicationContextRunner()
@@ -31,6 +34,14 @@ class CommandGatewayAutoConfigurationTest {
                 AssertionsForInterfaceTypes.assertThat(context)
                     .hasSingleBean(AggregateIdempotencyCheckerProvider::class.java)
                     .hasSingleBean(CommandGateway::class.java)
+                    .getBean(AggregateIdempotencyCheckerProvider::class.java)
+
+                context.getBean(AggregateIdempotencyCheckerProvider::class.java)
+                    .getChecker(MOCK_AGGREGATE_METADATA)
+                    .check(GlobalIdGenerator.generateAsString())
+                    .test()
+                    .expectNext(true)
+                    .verifyComplete()
             }
     }
 }
