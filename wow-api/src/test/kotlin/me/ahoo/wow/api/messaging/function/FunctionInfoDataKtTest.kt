a8df853f3e3bb995514c95627f272683a95948b5
Ahoo Wang <ahoowang@qq.com>
2024-07-26 10:27:57 +0800

    feat(core): add `FunctionInfo` API

--- wow-api/src/test/kotlin/me/ahoo/wow/api/messaging/function/FunctionInfoDataKtTest.kt
+++ wow-api/src/test/kotlin/me/ahoo/wow/api/messaging/function/FunctionInfoDataKtTest.kt
@@ -1,6 +1,5 @@
 package me.ahoo.wow.api.messaging.function
 
-import me.ahoo.wow.api.messaging.processor.materialize
 import org.hamcrest.MatcherAssert.*
 import org.hamcrest.Matchers.*
 import org.junit.jupiter.api.Test
