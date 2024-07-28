3a042aac38b03a5cc0074e9df0d62e3507bb5604
Ahoo Wang <ahoowang@qq.com>
2024-07-28 11:21:42 +0800

    feat(doc): add checkpoint to `CommandFunction`

--- wow-core/src/main/kotlin/me/ahoo/wow/modeling/command/CommandFunction.kt
+++ wow-core/src/main/kotlin/me/ahoo/wow/modeling/command/CommandFunction.kt
@@ -41,7 +41,9 @@ class CommandFunction<C : Any>(
     override fun invoke(exchange: ServerCommandExchange<*>): Mono<DomainEventStream> {
         return delegate
             .invoke(exchange)
-            .checkpoint("[${commandAggregate.aggregateId}] Invoke $qualifiedName Command[${exchange.message.id}] [CommandFunction]")
+            .checkpoint(
+                "[${commandAggregate.aggregateId}] Invoke $qualifiedName Command[${exchange.message.id}] [CommandFunction]"
+            )
             .map {
                 it.toDomainEventStream(exchange.message, commandAggregate.version)
             }
