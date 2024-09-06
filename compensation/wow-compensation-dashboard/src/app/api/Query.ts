e98ee633bba117ffc577173912c0df0e41beb5ba
Ahoo Wang <ahoowang@qq.com>
2024-09-06 16:41:28 +0800

    feat(dashboard): add NOR Operator (#914)

--- compensation/wow-compensation-dashboard/src/app/api/Query.ts
+++ compensation/wow-compensation-dashboard/src/app/api/Query.ts
@@ -1,6 +1,7 @@
 export enum Operator {
   AND = "AND",
   OR = "OR",
+  NOR = "NOR",
   ID = "ID",
   IDS = "IDS",
   DELETED = "DELETED",
@@ -54,7 +55,6 @@ export interface Condition {
   operator: Operator;
   value: any;
   children: Condition[];
-  not: boolean;
 }
 
 export enum SortDirection {
@@ -74,144 +74,138 @@ export interface Pagination {
 
 export class Conditions {
   static and(conditions: Condition[]): Condition {
-    return {field: "", operator: Operator.AND, value: "", children: conditions, not: false}
+    return {field: "", operator: Operator.AND, value: "", children: conditions}
   }
 
   static or(conditions: Condition[]): Condition {
-    return {field: "", operator: Operator.OR, value: "", children: conditions, not: false}
+    return {field: "", operator: Operator.OR, value: "", children: conditions}
+  }
+
+  static nor(conditions: Condition[]): Condition {
+    return {field: "", operator: Operator.NOR, value: "", children: conditions}
   }
 
   static id(value: string): Condition {
-    return {field: "", operator: Operator.ID, value: value, children: [], not: false}
+    return {field: "", operator: Operator.ID, value: value, children: []}
   }
 
   static ids(value: string[]): Condition {
-    return {field: "", operator: Operator.IDS, value: value, children: [], not: false}
+    return {field: "", operator: Operator.IDS, value: value, children: []}
   }
 
   static deleted(value: boolean): Condition {
-    return {field: "", operator: Operator.DELETED, value: value, children: [], not: false}
+    return {field: "", operator: Operator.DELETED, value: value, children: []}
   }
 
   static tenantId(value: string): Condition {
-    return {field: "", operator: Operator.TENANT_ID, value: value, children: [], not: false}
+    return {field: "", operator: Operator.TENANT_ID, value: value, children: []}
   }
 
   static all(): Condition {
-    return {field: "", operator: Operator.ALL, value: "", children: [], not: false}
+    return {field: "", operator: Operator.ALL, value: "", children: []}
   }
 
   static eq(field: string, value: any): Condition {
-    return {field, operator: Operator.EQ, value, children: [], not: false}
+    return {field, operator: Operator.EQ, value, children: []}
   }
 
   static ne(field: string, value: any): Condition {
-    return {field, operator: Operator.NE, value, children: [], not: false}
+    return {field, operator: Operator.NE, value, children: []}
   }
 
   static gt(field: string, value: any): Condition {
-    return {field, operator: Operator.GT, value, children: [], not: false}
+    return {field, operator: Operator.GT, value, children: []}
   }
 
   static lt(field: string, value: any): Condition {
-    return {field, operator: Operator.LT, value, children: [], not: false}
+    return {field, operator: Operator.LT, value, children: []}
   }
 
   static gte(field: string, value: any): Condition {
-    return {field, operator: Operator.GTE, value, children: [], not: false}
+    return {field, operator: Operator.GTE, value, children: []}
   }
 
   static lte(field: string, value: any): Condition {
-    return {field, operator: Operator.LTE, value, children: [], not: false}
+    return {field, operator: Operator.LTE, value, children: []}
   }
 
   static contains(field: string, value: any): Condition {
-    return {field, operator: Operator.CONTAINS, value, children: [], not: false}
+    return {field, operator: Operator.CONTAINS, value, children: []}
   }
 
   static in(field: string, value: any): Condition {
-    return {field, operator: Operator.IN, value, children: [], not: false}
+    return {field, operator: Operator.IN, value, children: []}
   }
 
   static notIn(field: string, value: any): Condition {
-    return {field, operator: Operator.NOT_IN, value, children: [], not: false}
+    return {field, operator: Operator.NOT_IN, value, children: []}
   }
 
   static between(field: string, start: any, end: any): Condition {
-    return {field, operator: Operator.BETWEEN, value: [start, end], children: [], not: false}
+    return {field, operator: Operator.BETWEEN, value: [start, end], children: []}
   }
 
   static allIn(field: string, value: any[]): Condition {
-    return {field, operator: Operator.ALL_IN, value, children: [], not: false}
+    return {field, operator: Operator.ALL_IN, value, children: []}
   }
 
   static startsWith(field: string, value: any): Condition {
-    return {field, operator: Operator.STARTS_WITH, value, children: [], not: false}
+    return {field, operator: Operator.STARTS_WITH, value, children: []}
   }
 
   static elemMatch(field: string, value: Condition): Condition {
-    return {field, operator: Operator.ELEM_MATCH, value: "", children: [value], not: false}
+    return {field, operator: Operator.ELEM_MATCH, value: "", children: [value]}
   }
 
   static isNull(field: string): Condition {
-    return {field, operator: Operator.NULL, value: "", children: [], not: false}
+    return {field, operator: Operator.NULL, value: "", children: []}
   }
 
   static notNull(field: string): Condition {
-    return {field, operator: Operator.NOT_NULL, value: "", children: [], not: false}
+    return {field, operator: Operator.NOT_NULL, value: "", children: []}
   }
 
   static isTrue(field: string): Condition {
-    return {field, operator: Operator.TRUE, value: "", children: [], not: false}
+    return {field, operator: Operator.TRUE, value: "", children: []}
   }
 
   static isFalse(field: string): Condition {
-    return {field, operator: Operator.FALSE, value: "", children: [], not: false}
+    return {field, operator: Operator.FALSE, value: "", children: []}
   }
 
   static today(field: string): Condition {
-    return {field, operator: Operator.TODAY, value: "", children: [], not: false}
+    return {field, operator: Operator.TODAY, value: "", children: []}
   }
 
   static tomorrow(field: string): Condition {
-    return {field, operator: Operator.TOMORROW, value: "", children: [], not: false}
+    return {field, operator: Operator.TOMORROW, value: "", children: []}
   }
 
   static thisWeek(field: string): Condition {
-    return {field, operator: Operator.THIS_WEEK, value: "", children: [], not: false}
+    return {field, operator: Operator.THIS_WEEK, value: "", children: []}
   }
 
   static nextWeek(field: string): Condition {
-    return {field, operator: Operator.NEXT_WEEK, value: "", children: [], not: false}
+    return {field, operator: Operator.NEXT_WEEK, value: "", children: []}
   }
 
   static lastWeek(field: string): Condition {
-    return {field, operator: Operator.LAST_WEEK, value: "", children: [], not: false}
+    return {field, operator: Operator.LAST_WEEK, value: "", children: []}
   }
 
   static thisMonth(field: string): Condition {
-    return {field, operator: Operator.THIS_MONTH, value: "", children: [], not: false}
+    return {field, operator: Operator.THIS_MONTH, value: "", children: []}
   }
 
   static lastMonth(field: string): Condition {
-    return {field, operator: Operator.LAST_MONTH, value: "", children: [], not: false}
+    return {field, operator: Operator.LAST_MONTH, value: "", children: []}
   }
 
   static recentDays(field: string, days: number): Condition {
-    return {field, operator: Operator.RECENT_DAYS, value: days, children: [], not: false}
+    return {field, operator: Operator.RECENT_DAYS, value: days, children: []}
   }
 
   static raw(raw: string): Condition {
-    return {field: "", operator: Operator.RAW, value: raw, children: [], not: false}
-  }
-
-  static not(condition: Condition): Condition {
-    return {
-      field: condition.field,
-      operator: condition.operator,
-      value: condition.value,
-      children: condition.children,
-      not: true
-    }
+    return {field: "", operator: Operator.RAW, value: raw, children: []}
   }
 }
