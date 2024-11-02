0613aab436d74a169d59b0d89903df99d81cfde5
Ahoo Wang <ahoowang@qq.com>
2024-11-02 19:15:33 +0800

    feat: Enhance `SyncSnapshotQueryApi` (#959)

--- example/example-api/build.gradle.kts
+++ example/example-api/build.gradle.kts
@@ -5,7 +5,10 @@ dependencies {
     api(platform(project(":wow-dependencies")))
     api("io.swagger.core.v3:swagger-core-jakarta")
     implementation(project(":wow-api"))
+    implementation(project(":wow-apiclient"))
     implementation("com.fasterxml.jackson.core:jackson-annotations")
     api("jakarta.validation:jakarta.validation-api")
+    api("me.ahoo.coapi:coapi-api")
+    implementation("org.springframework:spring-web")
     ksp(project(":wow-compiler"))
 }
