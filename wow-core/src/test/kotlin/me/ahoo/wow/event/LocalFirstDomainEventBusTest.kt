9296fe03d2f57a8341ee0dc8b5ab723ee18ebcf1
Ahoo Wang <ahoowang@qq.com>
2024-11-12 20:42:49 +0800

    feat(core): optimize LocalFirstMessageBus

--- wow-core/src/test/kotlin/me/ahoo/wow/event/LocalFirstDomainEventBusTest.kt
+++ wow-core/src/test/kotlin/me/ahoo/wow/event/LocalFirstDomainEventBusTest.kt
@@ -1,25 +1,47 @@
 package me.ahoo.wow.event
 
+import io.mockk.every
+import io.mockk.mockk
 import me.ahoo.wow.api.modeling.NamedAggregate
 import me.ahoo.wow.messaging.InMemoryMessageBus
 import me.ahoo.wow.tck.event.DomainEventBusSpec
 import org.junit.jupiter.api.Test
 import reactor.core.publisher.Sinks
+import reactor.core.publisher.Sinks.EmitResult
 import reactor.kotlin.test.test
 
 class LocalFirstDomainEventBusTest : DomainEventBusSpec() {
     override fun createMessageBus(): DomainEventBus {
         return LocalFirstDomainEventBus(
-            MockDistributedDomainEventBus(),
-            localBus = InMemoryDomainEventBus(sinkSupplier = {
-                Sinks.many().multicast().directBestEffort()
-            })
+            MockDistributedDomainEventBus()
         )
     }
 
     @Test
     fun sendMessageWhenNoSubscribers() {
-        val messageBus = createMessageBus()
+        val messageBus = LocalFirstDomainEventBus(
+            MockDistributedDomainEventBus(),
+            localBus = InMemoryDomainEventBus(sinkSupplier = {
+                Sinks.many().multicast().directBestEffort()
+            })
+        )
+        val message = createMessage()
+        messageBus.send(message)
+            .test()
+            .verifyComplete()
+    }
+
+    @Test
+    fun sendMessageWhenError() {
+        val messageBus = LocalFirstDomainEventBus(
+            MockDistributedDomainEventBus(),
+            localBus = InMemoryDomainEventBus(sinkSupplier = {
+                mockk<Sinks.Many<DomainEventStream>> {
+                    every { currentSubscriberCount() } returns 0
+                    every { tryEmitNext(any()) } returns EmitResult.FAIL_OVERFLOW
+                }
+            })
+        )
         val message = createMessage()
         messageBus.send(message)
             .test()
