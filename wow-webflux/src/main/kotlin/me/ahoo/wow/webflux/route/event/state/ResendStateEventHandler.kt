d76df044ab315e43caf6e70bac5832a62436c21e
Ahoo Wang <ahoowang@qq.com>
2024-11-17 11:05:16 +0800

    feat(webflux): Ensure batch processing execution sequence. (#1001)

--- wow-webflux/src/main/kotlin/me/ahoo/wow/webflux/route/event/state/ResendStateEventHandler.kt
+++ wow-webflux/src/main/kotlin/me/ahoo/wow/webflux/route/event/state/ResendStateEventHandler.kt
@@ -43,7 +43,7 @@ class ResendStateEventHandler(
     fun handle(afterId: String, limit: Int): Mono<BatchResult> {
         val target = CompensationTarget(function = RESEND_FUNCTION)
         return snapshotRepository.scanAggregateId(aggregateMetadata.namedAggregate, afterId, limit)
-            .flatMap { aggregateId ->
+            .concatMap { aggregateId ->
                 stateEventCompensator.resend(
                     aggregateId = aggregateId,
                     target = target,
