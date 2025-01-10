ed985db7b7d976870dd788c4268d32909581c6b3
Ahoo Wang <ahoowang@qq.com>
2025-01-10 09:40:50 +0800

    build: update Gradle plugins and dependencies
    - Update `publish-plugin` to `publish` in build.gradle.kts
    - Update `publish-plugin` to `publish` in gradle/libs.versions.toml

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,7 @@ import org.jetbrains.kotlin.gradle.dsl.KotlinJvmProjectExtension
 
 plugins {
     alias(libs.plugins.test.retry)
-    alias(libs.plugins.publish.plugin)
+    alias(libs.plugins.publish)
     alias(libs.plugins.detekt)
     alias(libs.plugins.kotlin)
     alias(libs.plugins.dokka)
