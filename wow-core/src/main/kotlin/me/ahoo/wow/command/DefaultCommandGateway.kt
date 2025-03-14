80d1d04a2371d562368028b1365c0311e30298f7
Ahoo Wang <ahoowang@qq.com>
2025-03-14 16:05:48 +0800

    refactor(wow-core): improve code readability in DefaultCommandGateway
    - Enhance code formatting for better visibility
    - Use proper indentation and line breaks to clarify structure
    
    Signed-off-by: Ahoo Wang <ahoowang@qq.com>

--- wow-core/src/main/kotlin/me/ahoo/wow/command/DefaultCommandGateway.kt
+++ wow-core/src/main/kotlin/me/ahoo/wow/command/DefaultCommandGateway.kt
@@ -138,12 +138,14 @@ class DefaultCommandGateway(
     }
 
     private fun Mono<Void>.thenEmitSentSignal(command: CommandMessage<*>, waitStrategy: WaitStrategy): Mono<Void> {
-        return then(Mono.fromRunnable {
-            val waitSignal = COMMAND_GATEWAY_FUNCTION.toWaitSignal(
-                commandId = command.commandId,
-                stage = CommandStage.SENT,
-            )
-            waitStrategy.next(waitSignal)
-        })
+        return then(
+            Mono.fromRunnable {
+                val waitSignal = COMMAND_GATEWAY_FUNCTION.toWaitSignal(
+                    commandId = command.commandId,
+                    stage = CommandStage.SENT,
+                )
+                waitStrategy.next(waitSignal)
+            }
+        )
     }
 }
