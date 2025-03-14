45e3efccd08a5b3c749f197894f3140d141d92d3
Ahoo Wang <ahoowang@qq.com>
2025-03-14 21:47:15 +0800

    fix(webflux): handle command request timeout and improve SSE response
    - Add timeout exception handling for command requests
    - Modify SSE data type to String and handle errors
    - Update responses to include error codes and improve error handling
    - Refactor test cases for better coverage
    
    Signed-off-by: Ahoo Wang <ahoowang@qq.com>

--- wow-webflux/src/test/kotlin/me/ahoo/wow/webflux/route/ResponsesKtTest.kt
+++ wow-webflux/src/test/kotlin/me/ahoo/wow/webflux/route/ResponsesKtTest.kt
@@ -1,21 +1,35 @@
-package me.ahoo.wow.webflux.exception
+/*
+ * Copyright [2021-present] [ahoo wang <ahoowang@qq.com> (https://github.com/Ahoo-Wang)].
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package me.ahoo.wow.webflux.route
 
 import io.mockk.every
 import io.mockk.mockk
+import me.ahoo.wow.api.exception.ErrorInfo
 import me.ahoo.wow.command.CommandResult
 import me.ahoo.wow.command.wait.CommandStage
 import me.ahoo.wow.exception.ErrorCodes
 import me.ahoo.wow.exception.toErrorInfo
 import me.ahoo.wow.id.generateGlobalId
 import me.ahoo.wow.openapi.command.CommandRequestHeaders.WOW_ERROR_CODE
-import me.ahoo.wow.webflux.route.toCommandResponse
-import me.ahoo.wow.webflux.route.toResponseEntity
-import me.ahoo.wow.webflux.route.toServerResponse
+import me.ahoo.wow.webflux.exception.DefaultRequestExceptionHandler
 import org.hamcrest.MatcherAssert.*
 import org.hamcrest.Matchers.*
 import org.junit.jupiter.api.Test
+import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
+import org.springframework.mock.web.reactive.function.server.MockServerRequest
 import org.springframework.web.reactive.function.server.ServerRequest
 import reactor.kotlin.core.publisher.toMono
 import reactor.kotlin.test.test
@@ -110,6 +124,30 @@ class ResponsesKtTest {
                 assertThat(it.statusCode(), equalTo(HttpStatus.OK))
                 assertThat(it.headers().contentType, equalTo(MediaType.TEXT_EVENT_STREAM))
                 assertThat(it.headers().getFirst(WOW_ERROR_CODE), equalTo(ErrorCodes.SUCCEEDED))
+            }
+            .verifyComplete()
+    }
+
+    @Test
+    fun `verify sse headers`() {
+        val mockRequest = MockServerRequest.builder()
+            .header(HttpHeaders.ACCEPT, MediaType.TEXT_EVENT_STREAM_VALUE)
+            .build()
+
+        CommandResult(
+            stage = CommandStage.SENT,
+            aggregateId = generateGlobalId(),
+            tenantId = generateGlobalId(),
+            requestId = generateGlobalId(),
+            commandId = generateGlobalId(),
+            contextName = "contextName",
+            processorName = "processorName",
+        ).toMono()
+            .toCommandResponse(mockRequest, DefaultRequestExceptionHandler)
+            .test()
+            .expectNextMatches { response ->
+                response.headers().contentType == MediaType.TEXT_EVENT_STREAM &&
+                    response.headers().getFirst(WOW_ERROR_CODE) == ErrorInfo.SUCCEEDED
             }
             .verifyComplete()
     }
