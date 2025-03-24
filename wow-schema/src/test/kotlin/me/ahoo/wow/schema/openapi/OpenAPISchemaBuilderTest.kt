a5e5490c52032e8be766cb0a1ffbcd16c7033175
Ahoo Wang <ahoowang@qq.com>
2025-03-24 17:10:01 +0800

    feat(schema): enhance schema naming strategy and support complex types
    - Improve schema naming strategy to handle nested and complex types
    - Add support for naming schemas with type parameters and arrays
    - Refactor type resolution and schema name generation for better readability
    - Update tests to cover new schema naming scenarios
    
    Signed-off-by: Ahoo Wang <ahoowang@qq.com>

--- wow-schema/src/test/kotlin/me/ahoo/wow/schema/openapi/OpenAPISchemaBuilderTest.kt
+++ wow-schema/src/test/kotlin/me/ahoo/wow/schema/openapi/OpenAPISchemaBuilderTest.kt
@@ -1,11 +1,14 @@
 package me.ahoo.wow.schema.openapi
 
+import me.ahoo.wow.api.query.MaterializedSnapshot
+import me.ahoo.wow.api.query.PagedList
 import me.ahoo.wow.example.api.cart.AddCartItem
 import me.ahoo.wow.example.api.order.CreateOrder
+import me.ahoo.wow.example.domain.order.OrderState
 import org.hamcrest.CoreMatchers.equalTo
 import org.hamcrest.CoreMatchers.notNullValue
 import org.hamcrest.CoreMatchers.nullValue
-import org.hamcrest.MatcherAssert.*
+import org.hamcrest.MatcherAssert.assertThat
 import org.junit.jupiter.api.Test
 
 class OpenAPISchemaBuilderTest {
@@ -17,9 +20,22 @@ class OpenAPISchemaBuilderTest {
         assertThat(createOderSchema.`$ref`, nullValue())
         val addCartItemSchema = openAPISchemaBuilder.generateSchema(AddCartItem::class.java)
         assertThat(addCartItemSchema.`$ref`, nullValue())
+        val orderStateSnapshotSchema = openAPISchemaBuilder.generateSchema(
+            MaterializedSnapshot::class.java,
+            OrderState::class.java
+        )
+        assertThat(orderStateSnapshotSchema.`$ref`, nullValue())
+        val orderStateSnapshotPagedListSchema = openAPISchemaBuilder.generateSchema(
+            PagedList::class.java,
+            openAPISchemaBuilder.resolveType(
+                MaterializedSnapshot::class.java,
+                OrderState::class.java
+            )
+        )
+        assertThat(orderStateSnapshotPagedListSchema.`$ref`, nullValue())
         val componentsSchemas = openAPISchemaBuilder.build()
         assertThat(createOderSchema.`$ref`, notNullValue())
         assertThat(addCartItemSchema.`$ref`, notNullValue())
-        assertThat(componentsSchemas.size, equalTo(4))
+        assertThat(componentsSchemas.size, equalTo(9))
     }
 }
