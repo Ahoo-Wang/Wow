53b02c7f83e04460a9eadd30ae792be7193e9585
Ahoo Wang <ahoowang@qq.com>
2025-03-24 11:42:38 +0800

    test(schema): add unit tests for WowSchemaDefinitionNamingStrategy
    - Create new test class WowSchemaDefinitionNamingStrategyTest
    - Add parameterized test for toSchemaName function
    - Test cases cover different scenarios including:
      - AggregateId class
      - CreateOrder class
      - Any class
      - WowSchemaDefinitionNamingStrategyTest class itself
    
    Signed-off-by: Ahoo Wang <ahoowang@qq.com>

---
+++ wow-schema/src/test/kotlin/me/ahoo/wow/schema/WowSchemaDefinitionNamingStrategyTest.kt
@@ -0,0 +1,34 @@
+package me.ahoo.wow.schema
+
+import io.swagger.v3.oas.annotations.media.Schema
+import me.ahoo.wow.api.modeling.AggregateId
+import me.ahoo.wow.example.api.order.CreateOrder
+import me.ahoo.wow.schema.WowSchemaDefinitionNamingStrategy.toSchemaName
+import org.hamcrest.CoreMatchers.equalTo
+import org.hamcrest.MatcherAssert.*
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+import java.util.stream.Stream
+
+@Schema(name = "SchemaDefinitionNamingStrategyTest")
+class WowSchemaDefinitionNamingStrategyTest {
+    companion object {
+        @JvmStatic
+        fun parametersForToSchemaName(): Stream<Arguments> {
+            return Stream.of(
+                Arguments.of(AggregateId::class.java, "wow.AggregateId"),
+                Arguments.of(CreateOrder::class.java, "example.order.CreateOrder"),
+                Arguments.of(Any::class.java, "Object"),
+                Arguments.of(WowSchemaDefinitionNamingStrategyTest::class.java, "SchemaDefinitionNamingStrategyTest")
+            )
+        }
+    }
+
+    @ParameterizedTest
+    @MethodSource("parametersForToSchemaName")
+    fun toSchemaName(clazz: Class<*>, expectedSchemaName: String) {
+        val schemaName = clazz.toSchemaName()
+        assertThat(schemaName, equalTo(expectedSchemaName))
+    }
+}
