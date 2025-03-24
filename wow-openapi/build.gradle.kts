2e385ea01da2bec29386267c7532cc1f0c1326c8
Ahoo Wang <ahoowang@qq.com>
2025-03-24 20:46:32 +0800

    feat(wow-openapi): add OpenAPIComponentContext and DefaultOpenAPIComponentContext
    - Introduce OpenAPIComponentContext interface and its default implementation
    - Add functionality to manage schemas, parameters, headers, request bodies, and responses
    - Implement inline schema capabilities
    - Update dependencies to include wow-schema
    
    Signed-off-by: Ahoo Wang <ahoowang@qq.com>

--- wow-openapi/build.gradle.kts
+++ wow-openapi/build.gradle.kts
@@ -3,6 +3,7 @@ description = "Wow OpenAPI Specification"
 dependencies {
     api(project(":wow-core"))
     api(project(":wow-query"))
+    api(project(":wow-schema"))
     implementation(project(":wow-bi"))
     implementation(kotlin("reflect"))
     implementation("org.springframework:spring-web")
