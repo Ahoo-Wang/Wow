6dc2f3924c84cc97ad547ae91ba874715a6bfc85
Ahoo Wang <ahoowang@qq.com>
2025-03-26 08:17:38 +0800

    feat(example): add mock path command to cart aggregate
    - Add MockPathCommand class to handle mock path requests
    - Update Cart class to include MockPathCommand in its aggregate root
    
    Signed-off-by: Ahoo Wang <ahoowang@qq.com>

---
+++ example/example-api/src/main/kotlin/me/ahoo/wow/example/api/cart/MockPathCommand.kt
@@ -0,0 +1,36 @@
+/*
+ * Copyright [2021-present] [ahoo wang <ahoowang@qq.com> (https://github.com/Ahoo-Wang)].
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package me.ahoo.wow.example.api.cart
+
+import me.ahoo.wow.api.annotation.CommandRoute
+import me.ahoo.wow.api.annotation.Order
+
+@Order(4)
+@CommandRoute(
+    method = CommandRoute.Method.POST,
+    appendIdPath = CommandRoute.AppendPath.ALWAYS,
+    appendTenantPath = CommandRoute.AppendPath.ALWAYS,
+    appendOwnerPath = CommandRoute.AppendPath.ALWAYS,
+    action = "{id}/{customerId}"
+)
+data class MockPathCommand(
+    @field:CommandRoute.PathVariable
+    val tenantId: String,
+    @field:CommandRoute.PathVariable
+    val ownerId: String,
+    @field:CommandRoute.PathVariable
+    val id: String,
+    @field:CommandRoute.PathVariable
+    val customerId: String
+)
