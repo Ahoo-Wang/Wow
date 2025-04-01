d0d594ffa7a70821e304988b104e6a64fa19000c
Ahoo Wang <ahoowang@qq.com>
2025-04-01 12:05:45 +0800

    refactor(infra-reflection): redesign annotation scanning mechanism
    - Introduce IntimateAnnotationElement class to encapsulate annotation scanning logic
    - Refactor AnnotationScanner to use the new IntimateAnnotationElement
    - Add support for repeatable annotations in Java
    - Improve performance by caching IntimateAnnotationElement instances
    - Remove redundant tests and simplify existing ones
    
    Signed-off-by: Ahoo Wang <ahoowang@qq.com>

---
+++ wow-core/src/main/kotlin/me/ahoo/wow/infra/reflection/IntimateAnnotationElement.kt
@@ -0,0 +1,108 @@
+/*
+ * Copyright [2021-present] [ahoo wang <ahoowang@qq.com> (https://github.com/Ahoo-Wang)].
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package me.ahoo.wow.infra.reflection
+
+import java.lang.reflect.Field
+import java.util.concurrent.ConcurrentHashMap
+import kotlin.reflect.KAnnotatedElement
+import kotlin.reflect.KMutableProperty
+import kotlin.reflect.KProperty
+import kotlin.reflect.full.hasAnnotation
+import kotlin.reflect.jvm.javaField
+
+/**
+ * IntimateAnnotationElement.
+ *
+ */
+class IntimateAnnotationElement(
+    val element: KAnnotatedElement,
+) {
+    val property: KProperty<*>? = element as? KProperty<*>
+    val getter: KProperty.Getter<*>? = property?.getter
+    val setter: KMutableProperty.Setter<*>? = if (property is KMutableProperty<*>) {
+        property.setter
+    } else {
+        null
+    }
+    val field: Field? = property?.javaField
+
+    val intimatedAnnotations: LinkedHashSet<Annotation> by lazy {
+        val merged = linkedSetOf<Annotation>()
+        merged.addAll(element.annotations)
+        if (getter != null) {
+            merged.addAll(getter.annotations)
+        }
+        if (setter != null) {
+            merged.addAll(setter.annotations)
+        }
+        if (field != null) {
+            merged.addAll(field.annotations)
+        }
+        merged.flatMap {
+            it.flatRepeatableAnnotation()
+        }
+        merged
+    }
+
+    val mergedAnnotations: Set<Annotation> by lazy {
+        val merged = linkedSetOf<Annotation>()
+        intimatedAnnotations.forEach {
+            it.inheritedAnnotations(merged)
+        }
+        merged
+    }
+
+    companion object {
+        private const val REPEATABLE_CONTAINER_SIMPLE_NAME = "Container"
+        private const val REPEATABLE_CONTAINER_ENDS_WITH = "${'$'}$REPEATABLE_CONTAINER_SIMPLE_NAME"
+
+        private val cache: ConcurrentHashMap<KAnnotatedElement, IntimateAnnotationElement> = ConcurrentHashMap()
+
+        fun KAnnotatedElement.toIntimateAnnotationElement(): IntimateAnnotationElement {
+            return cache.computeIfAbsent(this) {
+                IntimateAnnotationElement(it)
+            }
+        }
+
+        fun Annotation.flatRepeatableAnnotation(): List<Annotation> {
+            val containerClass = this.annotationClass.java
+            if (containerClass.simpleName == REPEATABLE_CONTAINER_SIMPLE_NAME &&
+                containerClass.name.endsWith(REPEATABLE_CONTAINER_ENDS_WITH)
+            ) {
+                try {
+                    @Suppress("UNCHECKED_CAST")
+                    val value = this.annotationClass.java.getMethod("value").invoke(this) as Array<Annotation>
+                    return value.toList()
+                } catch (ignore: Exception) {
+                    // ignore
+                }
+            }
+            return listOf(this)
+        }
+
+        fun Annotation.inheritedAnnotations(scanned: LinkedHashSet<Annotation> = linkedSetOf()): Set<Annotation> {
+            val existed = scanned.any { it == this }
+            if (existed) {
+                return scanned
+            }
+            scanned.add(this)
+            this.annotationClass.annotations.filter {
+                it.annotationClass.hasAnnotation<java.lang.annotation.Inherited>()
+            }.flatMap {
+                it.inheritedAnnotations(scanned)
+            }
+            return scanned
+        }
+    }
+}
---
+++ wow-core/src/test/kotlin/me/ahoo/wow/infra/reflection/IntimateAnnotationElementTest.kt
@@ -0,0 +1,184 @@
+package me.ahoo.wow.infra.reflection
+
+import me.ahoo.wow.api.annotation.OnMessage
+import me.ahoo.wow.api.messaging.function.FunctionKind
+import me.ahoo.wow.infra.reflection.IntimateAnnotationElement.Companion.toIntimateAnnotationElement
+import org.hamcrest.CoreMatchers.equalTo
+import org.hamcrest.MatcherAssert.*
+import org.junit.jupiter.api.Test
+import kotlin.reflect.jvm.javaField
+import kotlin.reflect.jvm.kotlinProperty
+
+class IntimateAnnotationElementTest {
+
+    @Test
+    fun getElement() {
+        val element = Data::property.toIntimateAnnotationElement()
+        assertThat(element.element, equalTo(Data::property))
+    }
+
+    @Test
+    fun getProperty() {
+        val element = Data::fieldProperty.toIntimateAnnotationElement()
+        assertThat(element.property, equalTo(Data::fieldProperty))
+    }
+
+    @Test
+    fun getGetter() {
+        val element = Data::fieldProperty.toIntimateAnnotationElement()
+        assertThat(element.getter, equalTo(Data::fieldProperty.getter))
+    }
+
+    @Test
+    fun getSetter() {
+        val element = Data::setProperty.toIntimateAnnotationElement()
+        assertThat(element.setter, equalTo(Data::setProperty.setter))
+    }
+
+    @Test
+    fun getField() {
+        val element = Data::fieldProperty.toIntimateAnnotationElement()
+        assertThat(element.field, equalTo(Data::fieldProperty.javaField))
+    }
+
+    @Test
+    fun getIntimatedAnnotations() {
+        val element = Data::property.toIntimateAnnotationElement()
+        assertThat(
+            element.intimatedAnnotations,
+            equalTo(linkedSetOf(MockAnnotation(), MockAnnotation(), MockAnnotation()))
+        )
+    }
+
+    @Test
+    fun getMergedAnnotations() {
+        val element = Data::fieldProperty.toIntimateAnnotationElement()
+        assertThat(
+            element.mergedAnnotations,
+            equalTo(linkedSetOf(MockAnnotation(), OnMessage(FunctionKind.EVENT, "hi")))
+        )
+    }
+
+    @Test
+    fun getIntimatedAnnotationsRepeatable() {
+        val element = Data::repeatable.toIntimateAnnotationElement()
+        assertThat(element.intimatedAnnotations, equalTo(linkedSetOf(MockAnnotation(), MockAnnotation())))
+    }
+
+    @Test
+    fun getJvmIntimatedAnnotationsRepeatable() {
+        val element = Data::jvmRepeatable.toIntimateAnnotationElement()
+        assertThat(
+            element.intimatedAnnotations,
+            equalTo(
+                linkedSetOf(
+                    JvmRepeatableTags(
+                        value = arrayOf(
+                            JvmRepeatableTag(value = "tag1"),
+                            JvmRepeatableTag(value = "tag2")
+                        )
+                    )
+                )
+            )
+        )
+    }
+
+    @Test
+    fun getJvm2IntimatedAnnotationsRepeatable() {
+        val element = Data::jvmRepeatable2.toIntimateAnnotationElement()
+        assertThat(
+            element.intimatedAnnotations,
+            equalTo(
+                linkedSetOf(
+                    JvmRepeatableTags(
+                        value = arrayOf(
+                            JvmRepeatableTag(value = "tag3"),
+                            JvmRepeatableTag(value = "tag4")
+                        )
+                    )
+                )
+            )
+        )
+    }
+
+    @Test
+    fun getJvmClassIntimatedAnnotationsRepeatable() {
+        val element =
+            MockRepeatableClass::class.java.getDeclaredField("field").kotlinProperty!!.toIntimateAnnotationElement()
+        assertThat(
+            element.intimatedAnnotations,
+            equalTo(
+                linkedSetOf(
+                    JvmRepeatableTags(
+                        value = arrayOf(
+                            JvmRepeatableTag(value = "tag1"),
+                            JvmRepeatableTag(value = "tag2")
+                        )
+                    )
+                )
+            )
+        )
+    }
+
+    @Test
+    fun getJvmClassIntimatedAnnotationsRepeatable2() {
+        val element =
+            MockRepeatableClass::class.java.getDeclaredField("field2").kotlinProperty!!.toIntimateAnnotationElement()
+        assertThat(
+            element.intimatedAnnotations,
+            equalTo(
+                linkedSetOf(
+                    JvmRepeatableTags(
+                        value = arrayOf(
+                            JvmRepeatableTag(value = "tag3"),
+                            JvmRepeatableTag(value = "tag4")
+                        )
+                    )
+                )
+            )
+        )
+    }
+}
+
+data class Data(
+    @MockAnnotation
+    @get:MockAnnotation
+    @field:MockAnnotation
+    val property: String,
+    @field:MockAnnotation
+    val fieldProperty: String,
+    @get:MockAnnotation
+    val getProperty: String,
+    @set:MockAnnotation
+    var setProperty: String,
+    @MockAnnotation
+    @MockAnnotation
+    val repeatable: String,
+    @field:JvmRepeatableTag("tag1")
+    @field:JvmRepeatableTag("tag2")
+    val jvmRepeatable: String,
+    @field:JvmRepeatableTags(
+        value = [
+            JvmRepeatableTag("tag3"),
+            JvmRepeatableTag("tag4")
+        ]
+    )
+    val jvmRepeatable2: String,
+    override val parentProperty: String,
+) : InterfaceAnnotation
+
+@Target(
+    AnnotationTarget.FIELD,
+    AnnotationTarget.PROPERTY,
+    AnnotationTarget.FUNCTION,
+    AnnotationTarget.PROPERTY_GETTER,
+    AnnotationTarget.PROPERTY_SETTER
+)
+@Repeatable
+@OnMessage(FunctionKind.EVENT, "hi")
+annotation class MockAnnotation
+
+interface InterfaceAnnotation {
+    @get:MockAnnotation
+    val parentProperty: String
+}
