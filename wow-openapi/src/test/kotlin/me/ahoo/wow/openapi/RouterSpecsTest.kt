053d5a83342d3b10f81fabc2260dd41bdacccf9b
Ahoo Wang <ahoowang@qq.com>
2025-05-19 16:55:31 +0800

    test(wow-openapi): add unit test for mergeOpenAPI functionality
    - Add new test case to verify mergeOpenAPI functionality
    - Import OpenAPI class for test implementation
    - Ensure that merged OpenAPI object has non-empty components and schemas
    
    Signed-off-by: Ahoo Wang <ahoowang@qq.com>

--- wow-openapi/src/test/kotlin/me/ahoo/wow/openapi/RouterSpecsTest.kt
+++ wow-openapi/src/test/kotlin/me/ahoo/wow/openapi/RouterSpecsTest.kt
@@ -1,5 +1,6 @@
 package me.ahoo.wow.openapi
 
+import io.swagger.v3.oas.models.OpenAPI
 import me.ahoo.test.asserts.assert
 import me.ahoo.wow.naming.MaterializedNamedBoundedContext
 import org.junit.jupiter.api.Test
@@ -9,5 +10,13 @@ class RouterSpecsTest {
     fun build() {
         val routerSpecs = RouterSpecs(MaterializedNamedBoundedContext("test")).build()
         routerSpecs.assert().isNotEmpty()
+    }
+
+    @Test
+    fun mergeOpenAPI() {
+        val openAPI = OpenAPI()
+        RouterSpecs(MaterializedNamedBoundedContext("test")).build()
+            .mergeOpenAPI(openAPI)
+        openAPI.components.schemas.assert().isNotEmpty()
     }
 }
