05a2165f5dcad8de561ebefbfb32276ae69cd8d9
Ahoo Wang <ahoowang@qq.com>
2025-06-18 11:32:40 +0800

    refactor(compensation): improve ApplyRetrySpecComponent and FailedListComponent
    - Update ApplyRetrySpecComponent to implement OnInit and use NonNullableFormBuilder
    - Remove unused controlToContainsCondition method in FailedListComponent
    - Add debugger statement in editRetrySpec method for debugging purposes
    
    Signed-off-by: Ahoo Wang <ahoowang@qq.com>

--- compensation/wow-compensation-dashboard/src/app/apply-retry-spec/apply-retry-spec.component.ts
+++ compensation/wow-compensation-dashboard/src/app/apply-retry-spec/apply-retry-spec.component.ts
@@ -38,20 +38,28 @@ import {NzInputNumberModule} from "ng-zorro-antd/input-number";
   templateUrl: './apply-retry-spec.component.html',
   styleUrl: './apply-retry-spec.component.scss'
 })
-export class ApplyRetrySpecComponent {
+export class ApplyRetrySpecComponent implements OnInit {
   @Input({required: true}) id!: string;
   @Input({required: true}) retrySpec!: RetrySpec;
   @Output() afterApply: EventEmitter<boolean> = new EventEmitter<boolean>();
-  validateForm = this.formBuilder.group({
-    maxRetries: [this.retrySpec.maxRetries, [Validators.required]],
-    minBackoff: [this.retrySpec.minBackoff, [Validators.required]],
-    executionTimeout: [this.retrySpec.executionTimeout, [Validators.required]]
-  });
+  validateForm!: FormGroup<{
+    maxRetries: FormControl<number>;
+    minBackoff: FormControl<number>;
+    executionTimeout: FormControl<number>;
+  }>
 
   constructor(private compensationClient: CompensationClient,
               private formBuilder: NonNullableFormBuilder,
               private message: NzMessageService) {
 
+  }
+
+  ngOnInit() {
+    this.validateForm = this.formBuilder.group({
+      maxRetries: [this.retrySpec.maxRetries, [Validators.required]],
+      minBackoff: [this.retrySpec.minBackoff, [Validators.required]],
+      executionTimeout: [this.retrySpec.executionTimeout, [Validators.required]]
+    });
   }
 
   applyRetrySpec() {
