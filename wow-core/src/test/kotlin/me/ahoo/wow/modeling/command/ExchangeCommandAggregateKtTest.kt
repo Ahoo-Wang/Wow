3153e6b9bf311dab02fbb2d489c45074023620dd
Ahoo Wang <ahoowang@qq.com>
2025-06-26 14:20:05 +0800

    test(wow-core): replace hamcrest with me.ahoo.test.asserts in tests
    - Remove hamcrest imports and usage from multiple test files
    - Replace with me.ahoo.test.asserts for assertions
    - Update test structure to use new assertion methods
    - Remove redundant code and simplify test logic
    
    Signed-off-by: Ahoo Wang <ahoowang@qq.com>

--- wow-core/src/test/kotlin/me/ahoo/wow/modeling/command/ExchangeCommandAggregateKtTest.kt
+++ wow-core/src/test/kotlin/me/ahoo/wow/modeling/command/ExchangeCommandAggregateKtTest.kt
@@ -1,10 +1,9 @@
 package me.ahoo.wow.modeling.command
 
 import io.mockk.mockk
+import me.ahoo.test.asserts.assert
 import me.ahoo.wow.api.command.CommandMessage
 import me.ahoo.wow.command.SimpleServerCommandExchange
-import org.hamcrest.MatcherAssert.*
-import org.hamcrest.Matchers.*
 import org.junit.jupiter.api.Test
 
 class ExchangeCommandAggregateKtTest {
@@ -12,6 +11,6 @@ class ExchangeCommandAggregateKtTest {
     @Test
     fun setCommandAggregate() {
         val exchange = SimpleServerCommandExchange(mockk<CommandMessage<*>>())
-        assertThat(exchange.setCommandAggregate(mockk()).getCommandAggregate<Any, Any>(), notNullValue())
+        exchange.setCommandAggregate(mockk()).getCommandAggregate<Any, Any>().assert().isNotNull()
     }
 }
