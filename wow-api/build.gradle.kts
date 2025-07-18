78bbd75ba295d20a739ec169f92a05995731c828
Ahoo Wang <ahoowang@qq.com>
2025-07-18 09:40:37 +0800

    feat(infra): add prepare key metadata and invocation handler
    - Add PreparableKey annotation for marking prepare key classes
    - Implement PrepareKeyInvocationHandler for handling prepare key method calls
    - Create PrepareKeyMetadata class to store prepare key metadata
    - Develop PrepareKeyMetadataParser for parsing prepare key metadata from classes
    - Update build.gradle.kts to include spring-context dependency
    
    Signed-off-by: Ahoo Wang <ahoowang@qq.com>

--- wow-api/build.gradle.kts
+++ wow-api/build.gradle.kts
@@ -1,4 +1,5 @@
 dependencies{
     compileOnly("com.fasterxml.jackson.core:jackson-annotations")
     compileOnly("io.swagger.core.v3:swagger-annotations-jakarta")
+    compileOnly("org.springframework:spring-context")
 }
---
+++ wow-core/src/main/kotlin/me/ahoo/wow/infra/prepare/PrepareKeyMetadataParser.kt
@@ -0,0 +1,40 @@
+/*
+ * Copyright [2021-present] [ahoo wang <ahoowang@qq.com> (https://github.com/Ahoo-Wang)].
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package me.ahoo.wow.infra.prepare
+
+import me.ahoo.wow.api.annotation.PreparableKey
+import me.ahoo.wow.command.annotation.CommandMetadataVisitor
+import me.ahoo.wow.infra.reflection.ClassMetadata.visit
+import me.ahoo.wow.infra.reflection.ClassVisitor
+import me.ahoo.wow.metadata.CacheableMetadataParser
+import me.ahoo.wow.metadata.Metadata
+
+object PrepareKeyMetadataParser : CacheableMetadataParser() {
+
+    override fun <TYPE : Any, M : Metadata> parseToMetadata(type: Class<TYPE>): M {
+        val visitor = CommandMetadataVisitor(type)
+        type.kotlin.visit(visitor)
+        @Suppress("UNCHECKED_CAST")
+        return visitor.toMetadata() as M
+    }
+}
+
+class PrepareKeyMetadataVisitor<P : Any>(private val prepareKeyType: Class<P>) : ClassVisitor<P> {
+
+
+    fun toMetadata(): PrepareKeyMetadata<P> {
+        val name = prepareKeyType.getAnnotation(PreparableKey::class.java)?.name ?: prepareKeyType.simpleName
+
+    }
+}
