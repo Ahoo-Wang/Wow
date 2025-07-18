33f15a0da1a2898e74e9c334ff805ee50fd0216f
Ahoo Wang <ahoowang@qq.com>
2025-07-18 09:40:37 +0800

    feat(infra): add prepare key metadata and invocation handler
    - Add PreparableKey annotation for marking prepare key classes
    - Implement PrepareKeyInvocationHandler for handling prepare key method calls
    - Create PrepareKeyMetadata class to store prepare key metadata
    - Develop PrepareKeyMetadataParser for parsing prepare key metadata from classes
    - Update build.gradle.kts to include spring-context dependency
    
    Signed-off-by: Ahoo Wang <ahoowang@qq.com>

---
+++ wow-core/src/main/kotlin/me/ahoo/wow/infra/prepare/PrepareKeyInvocationHandler.kt
@@ -0,0 +1,43 @@
+/*
+ * Copyright [2021-present] [ahoo wang <ahoowang@qq.com> (https://github.com/Ahoo-Wang)].
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package me.ahoo.wow.infra.prepare
+
+import me.ahoo.wow.infra.Decorator
+import java.lang.reflect.InvocationHandler
+import java.lang.reflect.Method
+
+class PrepareKeyInvocationHandler(
+    private val metadata: PrepareKeyMetadata,
+    override val delegate: PrepareKey<*>
+) : Decorator<PrepareKey<*>>, InvocationHandler {
+    companion object {
+        val EMPTY_ARGS = emptyArray<Any>()
+    }
+
+    private val declaredDefaultMethods by lazy {
+        metadata.proxyInterface.java.declaredMethods.filter { it.isDefault }
+    }
+
+    override fun invoke(
+        proxy: Any,
+        method: Method,
+        args: Array<out Any>?
+    ): Any? {
+        val methodArgs = args ?: EMPTY_ARGS
+        if (method.isDefault && declaredDefaultMethods.contains(method)) {
+            return InvocationHandler.invokeDefault(proxy, method, *methodArgs)
+        }
+        return method.invoke(delegate, *methodArgs)
+    }
+}
