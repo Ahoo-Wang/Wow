71840e8f6d5250a577008a84f2d77b50a4dae3db
Ahoo Wang <ahoowang@qq.com>
2025-07-21 16:19:00 +0800

    test(wow-test): implement AggregateSpec and GivenDsl for aggregate testing - Add AggregateSpec class for testing aggregates - Implement GivenDsl with given-when-then structure - Add support for forking verified stages - Update CartAggregateSpec to use new testing framework
    Signed-off-by: Ahoo Wang <ahoowang@qq.com>

---
+++ test/wow-test/src/main/kotlin/me/ahoo/wow/test/AggregateSpec.kt
@@ -0,0 +1,38 @@
+/*
+ * Copyright [2021-present] [ahoo wang <ahoowang@qq.com> (https://github.com/Ahoo-Wang)].
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package me.ahoo.wow.test
+
+import me.ahoo.wow.test.aggregate.AbstractDynamicTestBuilder
+import me.ahoo.wow.test.aggregate.AggregateDsl
+import org.junit.jupiter.api.DynamicNode
+import org.junit.jupiter.api.TestFactory
+import java.lang.reflect.ParameterizedType
+import java.util.stream.Stream
+
+abstract class AggregateSpec<C : Any, S : Any>(val block: AggregateDsl<C, S>.() -> Unit) :
+    AbstractDynamicTestBuilder() {
+    val commandAggregateType: Class<C>
+        get() {
+            val type = this::class.java.genericSuperclass as ParameterizedType
+            @Suppress("UNCHECKED_CAST")
+            return type.actualTypeArguments[0] as Class<C>
+        }
+
+    @TestFactory
+    fun execute(): Stream<DynamicNode> {
+        val aggregateDsl = AggregateDsl<C, S>(commandAggregateType)
+        block(aggregateDsl)
+        return aggregateDsl.dynamicNodes.stream()
+    }
+}
--- test/wow-test/src/main/kotlin/me/ahoo/wow/test/aggregate/AggregateDsl.kt
+++ test/wow-test/src/main/kotlin/me/ahoo/wow/test/aggregate/AggregateDsl.kt
@@ -18,11 +18,10 @@ import me.ahoo.wow.api.modeling.OwnerId
 import me.ahoo.wow.infra.Decorator
 import me.ahoo.wow.ioc.ServiceProvider
 import me.ahoo.wow.messaging.DefaultHeader
+import me.ahoo.wow.test.AggregateVerifier.aggregateVerifier
+import org.junit.jupiter.api.DynamicContainer
 import org.junit.jupiter.api.DynamicNode
-
-class AggregateDsl<S : Any> {
-
-}
+import org.junit.jupiter.api.DynamicTest
 
 interface DynamicTestBuilder {
     val dynamicNodes: List<DynamicNode>
@@ -32,10 +31,19 @@ abstract class AbstractDynamicTestBuilder : DynamicTestBuilder {
     override val dynamicNodes: MutableList<DynamicNode> = mutableListOf()
 }
 
-class GivenDsl<S : Any>(
-    private val delegate: GivenStage<S>
-) : AbstractDynamicTestBuilder() {
+class AggregateDsl<C : Any, S : Any>(private val commandAggregateType: Class<C>) :
+    AbstractDynamicTestBuilder() {
+    fun given(displayName: String = "Empty", block: GivenDsl<S>.() -> Unit) {
+        val givenStage = commandAggregateType.aggregateVerifier<C, S>()
+        val givenDsl = GivenDsl(givenStage)
+        block(givenDsl)
+        val dynamicTest = DynamicContainer.dynamicContainer("Given : $displayName", givenDsl.dynamicNodes)
+        dynamicNodes.add(dynamicTest)
+    }
+}
 
+
+abstract class AbstractGivenStageDsl<S : Any> : Decorator<GivenStage<S>>, AbstractDynamicTestBuilder() {
     fun inject(inject: ServiceProvider.() -> Unit) {
         delegate.inject(inject)
     }
@@ -50,7 +58,7 @@ class GivenDsl<S : Any>(
         dynamicNodes.addAll(whenDsl.dynamicNodes)
     }
 
-    fun givenEvent(events: Array<out Any>, block: WhenDsl<S>.() -> Unit) {
+    fun givenEvent(events: Array<out Any> = emptyArray(), block: WhenDsl<S>.() -> Unit) {
         val whenStage = delegate.givenEvent(*events)
         whenDsl(whenStage, block)
     }
@@ -60,11 +68,24 @@ class GivenDsl<S : Any>(
         whenDsl(whenStage, block)
     }
 
-    fun whenCommand(block: WhenDsl<S>.() -> Unit) {
-        val whenStage = delegate.givenEvent()
-        whenDsl(whenStage, block)
+    fun whenCommand(
+        command: Any,
+        header: Header = DefaultHeader.Companion.empty(),
+        ownerId: String = OwnerId.Companion.DEFAULT_OWNER_ID,
+        block: ExpectDsl<S>.() -> Unit
+    ) {
+        val givenStage = delegate.givenEvent()
+        val whenDsl = WhenDsl(givenStage)
+        whenDsl.whenCommand(command, header, ownerId, block)
+        dynamicNodes.addAll(whenDsl.dynamicNodes)
     }
 }
+
+class GivenDsl<S : Any>(
+    override val delegate: GivenStage<S>
+) : AbstractGivenStageDsl<S>()
+
+class ForkedVerifiedStageDsl<S : Any>(override val delegate: VerifiedStage<S>) : AbstractGivenStageDsl<S>()
 
 class WhenDsl<S : Any>(private val delegate: WhenStage<S>) : AbstractDynamicTestBuilder() {
     fun whenCommand(
@@ -76,17 +97,24 @@ class WhenDsl<S : Any>(private val delegate: WhenStage<S>) : AbstractDynamicTest
         val expectStage = delegate.whenCommand(command, header, ownerId)
         val expectDsl = ExpectDsl(expectStage)
         block(expectDsl)
+        val dynamicTest = DynamicTest.dynamicTest("Expect") {
+            expectStage.verify()
+        }
+        dynamicNodes.add(dynamicTest)
         dynamicNodes.addAll(expectDsl.dynamicNodes)
     }
 }
 
 class ExpectDsl<S : Any>(override val delegate: ExpectStage<S>) : ExpectStage<S> by delegate, Decorator<ExpectStage<S>>,
     AbstractDynamicTestBuilder() {
-    fun whenCommand(
-        block: GivenDsl<S>.() -> Unit
+    fun fork(
+        verifyError: Boolean = false,
+        block: ForkedVerifiedStageDsl<S>.() -> Unit
     ) {
-        val givenStage = delegate.verify().fork(true)
-        val expectStage = GivenDsl(givenStage)
-        block(expectStage)
+        val verifiedStage = delegate.verify().fork(verifyError)
+        val forkedVerifiedStageDsl = ForkedVerifiedStageDsl(verifiedStage)
+        block(forkedVerifiedStageDsl)
+        val forkNode = DynamicContainer.dynamicContainer("Fork", forkedVerifiedStageDsl.dynamicNodes)
+        dynamicNodes.add(forkNode)
     }
 }
