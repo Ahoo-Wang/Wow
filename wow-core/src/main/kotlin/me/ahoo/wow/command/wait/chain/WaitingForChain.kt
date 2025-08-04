1c6831b32932206598a0f985ae812b8f959e2b18
Ahoo Wang <ahoowang@qq.com>
2025-08-04 12:20:43 +0800

    feat(wow-core): add shouldPropagate method to WaitingForChain.Materialized
    - Implement the shouldPropagate method in the WaitingForChain.Materialized class
    - Add TODO placeholder for future implementation
    - This change prepares for integrating the waiting chain with the command chain
    
    Signed-off-by: Ahoo Wang <ahoowang@qq.com>

--- wow-core/src/main/kotlin/me/ahoo/wow/command/wait/chain/WaitingForChain.kt
+++ wow-core/src/main/kotlin/me/ahoo/wow/command/wait/chain/WaitingForChain.kt
@@ -14,6 +14,7 @@
 package me.ahoo.wow.command.wait.chain
 
 import me.ahoo.wow.api.messaging.Header
+import me.ahoo.wow.api.messaging.Message
 import me.ahoo.wow.command.wait.COMMAND_WAIT_PREFIX
 import me.ahoo.wow.command.wait.CommandStage
 import me.ahoo.wow.command.wait.WaitSignal
@@ -37,6 +38,11 @@ class WaitingForChain(override val materialized: Materialized) : WaitingFor() {
     class Materialized(
         val nodes: List<WaitingNode>,
     ) : WaitStrategy.Materialized {
+
+        override fun shouldPropagate(upstream: Message<*, *>): Boolean {
+            TODO("Not yet implemented")
+        }
+
         override fun propagate(commandWaitEndpoint: String, header: Header) {
             header.propagateCommandWaitEndpoint(commandWaitEndpoint)
                 .with(COMMAND_WAIT_CHAIN, nodes.toJsonString())
