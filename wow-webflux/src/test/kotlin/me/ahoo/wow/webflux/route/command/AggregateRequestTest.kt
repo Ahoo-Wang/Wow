801cf8bb1c0713c5d003a8a02c477e523800afe9
Ahoo Wang <ahoowang@qq.com>
2025-08-06 21:31:04 +0800

    test(wow-webflux): add unit tests for AggregateRequest
    - Add tests for various wait-related headers and their corresponding methods
    - Test tenant ID and aggregate ID retrieval from headers and path variables
    - Implement tests for wait timeout and local first functionality
    - Add tests for SSE (Server-Sent Events) detection
    - Implement tests for wait strategy extraction, including WaitingForStage and SimpleWaitingForChain
    
    Signed-off-by: Ahoo Wang <ahoowang@qq.com>

--- wow-webflux/src/test/kotlin/me/ahoo/wow/webflux/route/command/AggregateRequestTest.kt
+++ wow-webflux/src/test/kotlin/me/ahoo/wow/webflux/route/command/AggregateRequestTest.kt
@@ -13,14 +13,21 @@
 
 package me.ahoo.wow.webflux.route.command
 
+import io.mockk.every
+import io.mockk.mockk
 import me.ahoo.test.asserts.assert
 import me.ahoo.wow.api.annotation.AggregateRoute
+import me.ahoo.wow.api.command.CommandMessage
 import me.ahoo.wow.command.wait.CommandStage
+import me.ahoo.wow.command.wait.chain.SimpleWaitingChain
+import me.ahoo.wow.command.wait.chain.SimpleWaitingForChain
+import me.ahoo.wow.command.wait.stage.WaitingForStage
 import me.ahoo.wow.id.generateGlobalId
 import me.ahoo.wow.openapi.aggregate.command.CommandComponent
 import me.ahoo.wow.serialization.MessageRecords
 import org.junit.jupiter.api.Test
 import org.springframework.mock.web.reactive.function.server.MockServerRequest
+import java.time.Duration
 
 class AggregateRequestTest {
     @Test
@@ -87,5 +94,154 @@ class AggregateRequestTest {
         val request = MockServerRequest.builder()
             .header(CommandComponent.Header.WAIT_PROCESSOR, "test").build()
         request.getWaitProcessor().assert().isEqualTo("test")
+    }
+
+    @Test
+    fun getWaitFunction() {
+        val request = MockServerRequest.builder()
+            .header(CommandComponent.Header.WAIT_FUNCTION, "test").build()
+        request.getWaitFunction().assert().isEqualTo("test")
+    }
+
+    @Test
+    fun getWaitTailStage() {
+        val request = MockServerRequest.builder()
+            .header(CommandComponent.Header.WAIT_TAIL_STAGE, CommandStage.SENT.name).build()
+        request.getWaitTailStage().assert().isEqualTo(CommandStage.SENT)
+    }
+
+    @Test
+    fun getWaitTailStageIfNull() {
+        val request = MockServerRequest.builder().build()
+        request.getWaitTailStage().assert().isNull()
+    }
+
+    @Test
+    fun getWaitTailContext() {
+        val request = MockServerRequest.builder()
+            .header(CommandComponent.Header.WAIT_TAIL_CONTEXT, "test").build()
+        request.getWaitTailContext().assert().isEqualTo("test")
+    }
+
+    @Test
+    fun getWaitTailProcessor() {
+        val request = MockServerRequest.builder()
+            .header(CommandComponent.Header.WAIT_TAIL_PROCESSOR, "test").build()
+        request.getWaitTailProcessor().assert().isEqualTo("test")
+    }
+
+    @Test
+    fun getWaitTailFunction() {
+        val request = MockServerRequest.builder()
+            .header(CommandComponent.Header.WAIT_TAIL_FUNCTION, "test").build()
+        request.getWaitTailFunction().assert().isEqualTo("test")
+    }
+
+    @Test
+    fun getTenantId() {
+        val tenantId = generateGlobalId()
+        val request = MockServerRequest.builder()
+            .header(CommandComponent.Header.TENANT_ID, tenantId)
+            .build()
+        // We can't easily mock AggregateMetadata, so just test the header path
+        request.headers().firstHeader(CommandComponent.Header.TENANT_ID).assert().isEqualTo(tenantId)
+    }
+
+    @Test
+    fun getAggregateId() {
+        val aggregateId = generateGlobalId()
+        val request = MockServerRequest.builder()
+            .header(CommandComponent.Header.AGGREGATE_ID, aggregateId)
+            .build()
+        request.getAggregateId().assert().isEqualTo(aggregateId)
+    }
+
+    @Test
+    fun getLocalFirst() {
+        val request = MockServerRequest.builder()
+            .header(CommandComponent.Header.LOCAL_FIRST, "true")
+            .build()
+        request.getLocalFirst().assert().isEqualTo(true)
+    }
+
+    @Test
+    fun isSse() {
+        val request = MockServerRequest.builder()
+            .header("Accept", "text/event-stream")
+            .build()
+        request.isSse().assert().isTrue()
+    }
+
+    @Test
+    fun getWaitTimeout() {
+        val request = MockServerRequest.builder()
+            .header(CommandComponent.Header.WAIT_TIME_OUT, "5000")
+            .build()
+        request.getWaitTimeout().assert().isEqualTo(Duration.ofMillis(5000))
+    }
+
+    @Test
+    fun getWaitTimeoutWithDefault() {
+        val request = MockServerRequest.builder().build()
+        request.getWaitTimeout(Duration.ofSeconds(10)).assert().isEqualTo(Duration.ofSeconds(10))
+    }
+
+    @Test
+    fun getTenantIdFromPath() {
+        val tenantId = generateGlobalId()
+        val request = MockServerRequest.builder()
+            .pathVariable(MessageRecords.TENANT_ID, tenantId)
+            .build()
+        // We can't easily mock AggregateMetadata, so just test the path variable path
+        request.pathVariables()[MessageRecords.TENANT_ID].assert().isEqualTo(tenantId)
+    }
+
+    @Test
+    fun getAggregateIdFromPath() {
+        val aggregateId = generateGlobalId()
+        val request = MockServerRequest.builder()
+            .pathVariable(MessageRecords.ID, aggregateId)
+            .build()
+        request.getAggregateId().assert().isEqualTo(aggregateId)
+    }
+
+    @Test
+    fun extractWaitingForStage() {
+        val commandMessage = mockk<CommandMessage<Any>> {
+            every { commandId } returns generateGlobalId()
+            every { contextName } returns generateGlobalId()
+        }
+
+        val request = MockServerRequest.builder()
+            .header(CommandComponent.Header.WAIT_STAGE, CommandStage.SAGA_HANDLED.name)
+            .build()
+        val waitStrategy = request.extractWaitStrategy(commandMessage)
+        waitStrategy.assert().isInstanceOf(WaitingForStage::class.java)
+        waitStrategy.waitCommandId.assert().isEqualTo(commandMessage.commandId)
+        val waitingForStageMaterialized = waitStrategy.materialized as WaitingForStage.Materialized
+        waitingForStageMaterialized.function.assert().isNotNull()
+        waitingForStageMaterialized.stage.assert().isEqualTo(CommandStage.SAGA_HANDLED)
+        waitingForStageMaterialized.function!!.contextName.assert().isEqualTo(commandMessage.contextName)
+    }
+
+    @Test
+    fun extractSimpleWaitingForChain() {
+        val commandMessage = mockk<CommandMessage<Any>> {
+            every { commandId } returns generateGlobalId()
+            every { contextName } returns generateGlobalId()
+        }
+
+        val request = MockServerRequest.builder()
+            .header(CommandComponent.Header.WAIT_STAGE, CommandStage.SAGA_HANDLED.name)
+            .header(CommandComponent.Header.WAIT_TAIL_STAGE, CommandStage.PROJECTED.name)
+            .build()
+        val waitStrategy = request.extractWaitStrategy(commandMessage)
+        waitStrategy.assert().isInstanceOf(SimpleWaitingForChain::class.java)
+        waitStrategy.waitCommandId.assert().isEqualTo(commandMessage.commandId)
+        val simpleWaitingChain = waitStrategy.materialized as SimpleWaitingChain
+        simpleWaitingChain.function.assert().isNotNull()
+        simpleWaitingChain.function.contextName.assert().isEqualTo(commandMessage.contextName)
+        simpleWaitingChain.tail.stage.assert().isEqualTo(CommandStage.PROJECTED)
+        simpleWaitingChain.tail.function.contextName.assert().isEqualTo(commandMessage.contextName)
     }
 }
