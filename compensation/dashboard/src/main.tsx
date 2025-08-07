081fea85d871f8a24b3f114d17623cfcf816b9ce
Ahoo Wang <ahoowang@qq.com>
2025-08-07 15:25:58 +0800

    feat(dashboard): add failed view and integrate SWR
    - Add FailedSearch, FailedTable, and FailedView components
    - Integrate FailedView into the main application
    - Add SWR dependency for data fetching
    
    Signed-off-by: Ahoo Wang <ahoowang@qq.com>

--- compensation/dashboard/src/main.tsx
+++ compensation/dashboard/src/main.tsx
@@ -3,9 +3,11 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
+import { FailedView } from "./components/FailedView.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
+    <FailedView></FailedView>
   </StrictMode>,
 );
