ba1c57833c3066ab10908e9ac792a0ad0e3cc750
Ahoo Wang <ahoowang@qq.com>
2025-10-15 15:57:50 +0800

    refactor(schema): simplify WowJacksonModule and enhance SchemaGeneratorBuilder
    - Removed unused imports and nested annotation check logic
    - Simplified shouldIgnoreField method in WowJacksonModule
    - Added documentation comments to SchemaGeneratorBuilder properties
    - Enhanced build method documentation in SchemaGeneratorBuilder
    - Fixed indentation in SchemaGeneratorTest
    
    Signed-off-by: Ahoo Wang <ahoowang@qq.com>

--- wow-schema/src/main/kotlin/me/ahoo/wow/schema/jackson/WowJacksonModule.kt
+++ wow-schema/src/main/kotlin/me/ahoo/wow/schema/jackson/WowJacksonModule.kt
@@ -13,56 +13,16 @@
 
 package me.ahoo.wow.schema.jackson
 
-import com.fasterxml.jackson.annotation.JacksonAnnotationsInside
-import com.fasterxml.jackson.annotation.JsonBackReference
-import com.fasterxml.jackson.annotation.JsonUnwrapped
-import com.fasterxml.jackson.databind.ObjectMapper
 import com.github.victools.jsonschema.generator.FieldScope
-import com.github.victools.jsonschema.generator.SchemaGeneratorConfigBuilder
 import com.github.victools.jsonschema.module.jackson.JacksonModule
 import com.github.victools.jsonschema.module.jackson.JacksonOption
 
 class WowJacksonModule(vararg options: JacksonOption) : JacksonModule(*options) {
-    companion object {
-        val NESTED_ANNOTATION_CHECK: (Annotation) -> Boolean = { annotation ->
-            annotation.javaClass.isAnnotationPresent(JacksonAnnotationsInside::class.java)
-        }
-    }
-
-    private lateinit var objectMapper: ObjectMapper
-
-    override fun applyToConfigBuilder(builder: SchemaGeneratorConfigBuilder) {
-        this.objectMapper = builder.objectMapper
-        super.applyToConfigBuilder(builder)
-    }
 
     override fun shouldIgnoreField(field: FieldScope): Boolean {
-        if (field.getAnnotationConsideringFieldAndGetterIfSupported(
-                JsonBackReference::class.java,
-                NESTED_ANNOTATION_CHECK
-            ) != null
-        ) {
-            return true
+        if (field.type.erasedType == Boolean::class.java && field.name.startsWith("is")) {
+            return false
         }
-
-        // @since 4.32.0
-        val unwrappedAnnotation = field.getAnnotationConsideringFieldAndGetterIfSupported<JsonUnwrapped?>(
-            JsonUnwrapped::class.java,
-            NESTED_ANNOTATION_CHECK
-        )
-        if (unwrappedAnnotation != null && unwrappedAnnotation.enabled) {
-            // unwrapped properties should be ignored here, as they are included in their unwrapped form
-            return true
-        }
-
-        // instead of re-creating the various ways a property may be included/excluded in jackson: just use its built-in introspection
-        val topMostHierarchyType = field.declaringTypeMembers.allTypesAndOverrides()[0]
-        val beanDescription = this.getBeanDescriptionForClass(topMostHierarchyType.type)
-
-        // some kinds of field ignorals are only available via an annotation introspector
-        val ignoredProperties = this.objectMapper.serializationConfig.annotationIntrospector
-            .findPropertyIgnoralByName(null, beanDescription.classInfo).ignored
-        val declaredName = field.declaredName
-        return ignoredProperties.contains(declaredName)
+        return super.shouldIgnoreField(field)
     }
 }
