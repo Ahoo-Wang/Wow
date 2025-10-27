78381574fff4479e55e5cbed7096ada92b489d0e
Ahoo Wang <ahoowang@qq.com>
2025-10-27 11:56:17 +0800

    build(dashboard): add babel-plugin-react-compiler to vite config
    - Add babel-plugin-react-compiler as a dependency
    - Configure react plugin in vite to use the new babel plugin
    - Update pnpm lock file with new dependency
    - Simplify string manipulation in QuickNavigation.kt using dropLast
    
    Signed-off-by: Ahoo Wang <ahoowang@qq.com>

--- compensation/dashboard/vite.config.ts
+++ compensation/dashboard/vite.config.ts
@@ -5,5 +5,12 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react({
+      babel: {
+        plugins: ["babel-plugin-react-compiler"],
+      },
+    }),
+    tailwindcss(),
+  ],
 });
