915340be0ef3de855736bd73cf06b88b59aabc03
Ahoo Wang <ahoowang@qq.com>
2025-11-26 17:23:19 +0800

    refactor(api): rename targetOwnerId to toOwnerId in OwnerTransferred
    - Renamed property targetOwnerId to toOwnerId in OwnerTransferred interface
    - Updated implementation in SimpleStateAggregate to use toOwnerId
    - Adjusted test cases to reflect the property name change
    
    Signed-off-by: Ahoo Wang <ahoowang@qq.com>

--- wow-api/src/main/kotlin/me/ahoo/wow/api/event/OwnerTransferred.kt
+++ wow-api/src/main/kotlin/me/ahoo/wow/api/event/OwnerTransferred.kt
@@ -19,9 +19,9 @@ package me.ahoo.wow.api.event
  * This interface is typically implemented by domain events that signal a change in ownership,
  * such as transferring responsibility or control of an entity to another party.
  *
- * @property targetOwnerId The unique identifier of the new owner to whom ownership is being transferred.
+ * @property toOwnerId The unique identifier of the new owner to whom ownership is being transferred.
  *                         This should be a non-empty string representing the owner's ID.
  */
 interface OwnerTransferred {
-    val targetOwnerId: String
+    val toOwnerId: String
 }
