bcd349209d5a7e08545c86a35123edc99d8de72e
Ahoo Wang <ahoowang@qq.com>
2026-01-05 15:32:41 +0800

    feat(opentelemetry): add tracing support for wait strategy
    - Implemented TracingWaitStrategy to wrap delegate wait strategy with OpenTelemetry tracing
    - Added WaitStrategyInstrumenter for creating OpenTelemetry instrumenter for wait operations
    - Created WaitStrategySpaceNameExtractor for span name extraction
    - Integrated tracing functionality into existing Tracing extension methods
    - Added support for tracing wait operations in command waiting flows
    - Extended tracing wrapper to handle WaitStrategy instances
    
    Signed-off-by: Ahoo Wang <ahoowang@qq.com>

---
+++ wow-opentelemetry/src/main/kotlin/me/ahoo/wow/opentelemetry/wait/WaitStrategyInstrumenter.kt
@@ -0,0 +1,25 @@
+package me.ahoo.wow.opentelemetry.wait
+
+import io.opentelemetry.api.GlobalOpenTelemetry
+import io.opentelemetry.instrumentation.api.instrumenter.Instrumenter
+import io.opentelemetry.instrumentation.api.instrumenter.SpanNameExtractor
+import me.ahoo.wow.api.Wow
+import me.ahoo.wow.opentelemetry.WowInstrumenter.INSTRUMENTATION_NAME_PREFIX
+
+object WaitStrategyInstrumenter {
+    private const val INSTRUMENTATION_NAME = "${INSTRUMENTATION_NAME_PREFIX}wait"
+
+    val INSTRUMENTER: Instrumenter<String, Unit> =
+        Instrumenter.builder<String, Unit>(
+            GlobalOpenTelemetry.get(),
+            INSTRUMENTATION_NAME,
+            WaitStrategySpaceNameExtractor,
+        ).setInstrumentationVersion(Wow.VERSION)
+            .buildInstrumenter()
+}
+
+object WaitStrategySpaceNameExtractor : SpanNameExtractor<String> {
+    override fun extract(request: String): String {
+        return request
+    }
+}
