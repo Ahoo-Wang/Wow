7a6a8cb7329f473d1f27642960baa9c6c4f827b0
Ahoo Wang <ahoowang@qq.com>
2026-01-05 21:39:55 +0800

    perf(core): add JMH benchmarks for command processing
    - Added GlobalIdBenchmark to measure ID generation performance
    - Created InMemoryCommandBusBenchmark to test command bus throughput
    - Integrated JMH dependencies for performance testing infrastructure
    - Added benchmark setup for command message sending operations
    - Included TCK and test project dependencies for benchmark execution
    
    Signed-off-by: Ahoo Wang <ahoowang@qq.com>

--- wow-core/build.gradle.kts
+++ wow-core/build.gradle.kts
@@ -14,4 +14,6 @@ dependencies {
     implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
     compileOnly("io.swagger.core.v3:swagger-annotations-jakarta")
     testImplementation(project(":wow-tck"))
+    jmh(project(":wow-test"))
+    jmh(project(":wow-tck"))
 }
