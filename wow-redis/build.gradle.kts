169816e16c3680dbe646cd6781d9a880cbac5893
Ahoo Wang <ahoowang@qq.com>
2026-01-07 19:12:57 +0800

    refactor(wow-redis): change lettuce-core dependency scope from implementation to api
    - Updated lettuce-core dependency from implementation to api scope
    - This allows the lettuce-core classes to be exposed as part of the public API
    - Enables downstream projects to access lettuce-core functionality directly
    
    Signed-off-by: Ahoo Wang <ahoowang@qq.com>

--- wow-redis/build.gradle.kts
+++ wow-redis/build.gradle.kts
@@ -1,6 +1,6 @@
 dependencies {
     api(project(":wow-core"))
     api("org.springframework.data:spring-data-redis")
-    implementation("io.lettuce:lettuce-core")
+    api("io.lettuce:lettuce-core")
     testImplementation(project(":wow-tck"))
 }
