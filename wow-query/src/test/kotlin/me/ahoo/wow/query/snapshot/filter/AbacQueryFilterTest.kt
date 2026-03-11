b8161ff393697447ad49fe30a2487b8b5f411c79
Ahoo Wang <ahoowang@qq.com>
2026-03-11 14:17:16 +0800

    test(query): add comprehensive tests for AbacQueryFilter implementation
    - Added imports for mockk, Mono, and test utilities
    - Included EMPTY_ABAC_TAGS and Condition imports for testing
    - Added EmptyAbacQueryFilter test with empty principal tags scenario
    - Added MockAbacQueryFilter test with sample department and role tags
    - Created test cases using DefaultQueryContext and mocked FilterChain
    - Verified complete execution of both filter scenarios using reactor test
    - Added object implementations for EmptyAbacQueryFilter and MockAbacQueryFilter
    - Tested count query type with MOCK_AGGREGATE_METADATA context
    
    Signed-off-by: Ahoo Wang <ahoowang@qq.com>

--- wow-query/src/test/kotlin/me/ahoo/wow/query/snapshot/filter/AbacQueryFilterTest.kt
+++ wow-query/src/test/kotlin/me/ahoo/wow/query/snapshot/filter/AbacQueryFilterTest.kt
@@ -13,14 +13,26 @@
 
 package me.ahoo.wow.query.snapshot.filter
 
+import io.mockk.every
+import io.mockk.mockk
 import me.ahoo.test.asserts.assert
 import me.ahoo.wow.api.abac.AbacTagKey
 import me.ahoo.wow.api.abac.AbacTagValue
 import me.ahoo.wow.api.abac.AbacTags
+import me.ahoo.wow.api.abac.EMPTY_ABAC_TAGS
 import me.ahoo.wow.api.abac.wildcard
+import me.ahoo.wow.api.query.Condition
 import me.ahoo.wow.api.query.Operator
+import me.ahoo.wow.filter.FilterChain
+import me.ahoo.wow.query.filter.DefaultQueryContext
+import me.ahoo.wow.query.filter.QueryContext
+import me.ahoo.wow.query.filter.QueryType
 import me.ahoo.wow.query.snapshot.filter.AbacQueryFilter.Companion.toCondition
+import me.ahoo.wow.tck.mock.MOCK_AGGREGATE_METADATA
 import org.junit.jupiter.api.Test
+import reactor.core.publisher.Mono
+import reactor.kotlin.test.test
+import reactor.util.context.ContextView
 
 class AbacQueryFilterTest {
     @Test
@@ -97,5 +109,48 @@ class AbacQueryFilterTest {
 
         condition.assert().isNotNull()
         condition.operator.assert().isEqualTo(Operator.OR)
+    }
+
+    @Test
+    fun `filter for EmptyAbacQueryFilter`() {
+        val context = DefaultQueryContext<Condition, Any>(
+            queryType = QueryType.COUNT,
+            MOCK_AGGREGATE_METADATA
+        )
+        val chain = mockk<FilterChain<QueryContext<*, *>>> {
+            every {
+                filter(context)
+            } returns Mono.empty()
+        }
+        EmptyAbacQueryFilter.filter(context, chain).test().verifyComplete()
+    }
+
+    @Test
+    fun `filter for MockAbacQueryFilter`() {
+        val context = DefaultQueryContext<Condition, Any>(
+            queryType = QueryType.COUNT,
+            MOCK_AGGREGATE_METADATA
+        ).setQuery(Condition.ALL)
+        val chain = mockk<FilterChain<QueryContext<*, *>>> {
+            every {
+                filter(context)
+            } returns Mono.empty()
+        }
+        MockAbacQueryFilter.filter(context, chain).test().verifyComplete()
+    }
+
+    object EmptyAbacQueryFilter : AbacQueryFilter() {
+        override fun ContextView.getPrincipalTags(context: QueryContext<*, *>): AbacTags {
+            return EMPTY_ABAC_TAGS
+        }
+    }
+
+    object MockAbacQueryFilter : AbacQueryFilter() {
+        override fun ContextView.getPrincipalTags(context: QueryContext<*, *>): AbacTags {
+            return mapOf(
+                "dept" to listOf("eng"),
+                "role" to listOf("admin"),
+            )
+        }
     }
 }
