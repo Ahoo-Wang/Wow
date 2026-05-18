527d7b64a8c58b589a63af2c6a062fa6a1cb0c3c
Ahoo Wang <ahoowang@qq.com>
2026-05-18 13:48:13 +0800

    test: add KDoc and comprehensive unit tests for RecoverableExceptionRegistrar (#2629)
    - Add KDoc comments to RecoverableExceptionProvider interface and RecoverableExceptionRegistrar object
    - Add test SPI provider (TestRecoverableExceptionProvider) with META-INF/services config
    - Expand tests from 1 to 13 cases covering: register/unregister, overwrite, default rules,
      Class/Throwable extension properties, SPI loading via ServiceLoader

--- wow-core/src/test/kotlin/me/ahoo/wow/exception/RecoverableExceptionRegistrarTest.kt
+++ wow-core/src/test/kotlin/me/ahoo/wow/exception/RecoverableExceptionRegistrarTest.kt
@@ -1,7 +1,21 @@
+/*
+ * Copyright [2021-present] [ahoo wang <ahoowang@qq.com> (https://github.com/Ahoo-Wang)].
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package me.ahoo.wow.exception
 
 import me.ahoo.test.asserts.assert
 import me.ahoo.wow.api.exception.RecoverableType
+import me.ahoo.wow.exception.RecoverableExceptionRegistrar.getRecoverableType
 import me.ahoo.wow.exception.RecoverableExceptionRegistrar.register
 import me.ahoo.wow.exception.RecoverableExceptionRegistrar.unregister
 import org.junit.jupiter.api.Test
@@ -10,16 +24,89 @@ import java.util.concurrent.TimeoutException
 class RecoverableExceptionRegistrarTest {
 
     @Test
-    fun `should recoverable`() {
+    fun `getRecoverableType returns null when not registered`() {
+        getRecoverableType(IllegalStateException::class.java).assert().isNull()
+    }
+
+    @Test
+    fun `register and getRecoverableType`() {
         register(IllegalArgumentException::class.java, RecoverableType.UNRECOVERABLE)
-        IllegalArgumentException::class.java.recoverable.assert().isEqualTo(RecoverableType.UNRECOVERABLE)
+        getRecoverableType(IllegalArgumentException::class.java).assert().isEqualTo(RecoverableType.UNRECOVERABLE)
+        unregister(IllegalArgumentException::class.java)
+    }
+
+    @Test
+    fun `register overwrites previous mapping`() {
+        register(IllegalArgumentException::class.java, RecoverableType.UNRECOVERABLE)
+        getRecoverableType(IllegalArgumentException::class.java).assert().isEqualTo(RecoverableType.UNRECOVERABLE)
+
+        register(IllegalArgumentException::class.java, RecoverableType.RECOVERABLE)
+        getRecoverableType(IllegalArgumentException::class.java).assert().isEqualTo(RecoverableType.RECOVERABLE)
+
+        unregister(IllegalArgumentException::class.java)
+    }
+
+    @Test
+    fun `unregister removes mapping`() {
+        register(IllegalArgumentException::class.java, RecoverableType.UNRECOVERABLE)
+        getRecoverableType(IllegalArgumentException::class.java).assert().isNotNull()
+
+        unregister(IllegalArgumentException::class.java)
+        getRecoverableType(IllegalArgumentException::class.java).assert().isNull()
+    }
+
+    @Test
+    fun `unregister is no-op for unregistered class`() {
+        unregister(IllegalStateException::class.java)
+        getRecoverableType(IllegalStateException::class.java).assert().isNull()
+    }
+
+    @Test
+    fun `default recoverable for RecoverableException is RECOVERABLE`() {
+        MockRecoverableException().recoverable.assert().isEqualTo(RecoverableType.RECOVERABLE)
+    }
+
+    @Test
+    fun `default recoverable for TimeoutException is RECOVERABLE`() {
+        TimeoutException::class.java.recoverable.assert().isEqualTo(RecoverableType.RECOVERABLE)
+    }
+
+    @Test
+    fun `default recoverable for unknown exception is UNKNOWN`() {
+        RuntimeException::class.java.recoverable.assert().isEqualTo(RecoverableType.UNKNOWN)
+    }
+
+    @Test
+    fun `Throwable recoverable delegates to Class recoverable`() {
+        register(IllegalArgumentException::class.java, RecoverableType.UNRECOVERABLE)
+        IllegalArgumentException().recoverable.assert().isEqualTo(RecoverableType.UNRECOVERABLE)
         unregister(IllegalArgumentException::class.java)
 
-        IllegalArgumentException::class.java.recoverable.assert().isEqualTo(RecoverableType.UNKNOWN)
+        IllegalArgumentException().recoverable.assert().isEqualTo(RecoverableType.UNKNOWN)
+    }
 
+    @Test
+    fun `explicit registration takes precedence over default rules`() {
+        register(TimeoutException::class.java, RecoverableType.UNRECOVERABLE)
+        TimeoutException::class.java.recoverable.assert().isEqualTo(RecoverableType.UNRECOVERABLE)
+
+        unregister(TimeoutException::class.java)
         TimeoutException::class.java.recoverable.assert().isEqualTo(RecoverableType.RECOVERABLE)
+    }
 
-        MockRecoverableException().recoverable.assert().isEqualTo(RecoverableType.RECOVERABLE)
+    @Test
+    fun `SPI provider is loaded via ServiceLoader`() {
+        getRecoverableType(TestSpiException::class.java).assert().isEqualTo(RecoverableType.RECOVERABLE)
+    }
+
+    @Test
+    fun `SPI registered exception is accessible via Class recoverable extension`() {
+        TestSpiException::class.java.recoverable.assert().isEqualTo(RecoverableType.RECOVERABLE)
+    }
+
+    @Test
+    fun `SPI registered exception is accessible via Throwable recoverable extension`() {
+        TestSpiException().recoverable.assert().isEqualTo(RecoverableType.RECOVERABLE)
     }
 
     class MockRecoverableException : RecoverableException, RuntimeException()
