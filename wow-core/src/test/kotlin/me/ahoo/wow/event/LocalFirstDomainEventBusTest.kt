bae4f2724c235ed90ac16b7ceb874aaed507dd8c
Ahoo Wang <ahoowang@qq.com>
2026-06-04 19:26:42 +0800

    fix(core): harden local bus subscriptions (#2660)

--- wow-core/src/test/kotlin/me/ahoo/wow/event/LocalFirstDomainEventBusTest.kt
+++ wow-core/src/test/kotlin/me/ahoo/wow/event/LocalFirstDomainEventBusTest.kt
@@ -2,19 +2,38 @@ package me.ahoo.wow.event
 
 import io.mockk.every
 import io.mockk.mockk
+import me.ahoo.test.asserts.assert
 import me.ahoo.wow.api.modeling.NamedAggregate
 import me.ahoo.wow.messaging.InMemoryMessageBus
 import me.ahoo.wow.tck.event.DomainEventBusSpec
 import org.junit.jupiter.api.Test
+import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
 import reactor.core.publisher.Sinks
 import reactor.core.publisher.Sinks.EmitResult
 import reactor.kotlin.test.test
+import java.util.concurrent.atomic.AtomicBoolean
 
 class LocalFirstDomainEventBusTest : DomainEventBusSpec() {
     override fun createMessageBus(): DomainEventBus {
         return LocalFirstDomainEventBus(
             MockDistributedDomainEventBus()
         )
+    }
+
+    @Test
+    fun `should close local and distributed buses`() {
+        val distributedBus = RecordingDistributedDomainEventBus()
+        val localBus = RecordingLocalDomainEventBus()
+        val messageBus = LocalFirstDomainEventBus(
+            distributedBus = distributedBus,
+            localBus = localBus,
+        )
+
+        messageBus.close()
+
+        localBus.closed.get().assert().isTrue()
+        distributedBus.closed.get().assert().isTrue()
     }
 
     @Test
@@ -56,5 +75,31 @@ class MockDistributedDomainEventBus(
 ) : DistributedDomainEventBus, InMemoryMessageBus<DomainEventStream, EventStreamExchange>() {
     override fun DomainEventStream.createExchange(): EventStreamExchange {
         return SimpleEventStreamExchange(this)
+    }
+}
+
+private class RecordingLocalDomainEventBus : LocalDomainEventBus {
+    val closed = AtomicBoolean()
+
+    override fun send(message: DomainEventStream): Mono<Void> = Mono.empty()
+
+    override fun receive(namedAggregates: Set<NamedAggregate>): Flux<EventStreamExchange> = Flux.empty()
+
+    override fun subscriberCount(namedAggregate: NamedAggregate): Int = 0
+
+    override fun close() {
+        closed.set(true)
+    }
+}
+
+private class RecordingDistributedDomainEventBus : DistributedDomainEventBus {
+    val closed = AtomicBoolean()
+
+    override fun send(message: DomainEventStream): Mono<Void> = Mono.empty()
+
+    override fun receive(namedAggregates: Set<NamedAggregate>): Flux<EventStreamExchange> = Flux.empty()
+
+    override fun close() {
+        closed.set(true)
     }
 }
