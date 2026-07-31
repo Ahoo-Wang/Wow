9f9917a528f742c389a63056de91a9e643fed3da
Ahoo Wang <ahoowang@qq.com>
2026-07-31 11:20:27 +0800

    fix(test): wait for Elasticsearch authentication readiness

--- test/wow-tck/src/main/kotlin/me/ahoo/wow/tck/container/WowTestContainers.kt
+++ test/wow-tck/src/main/kotlin/me/ahoo/wow/tck/container/WowTestContainers.kt
@@ -16,15 +16,40 @@ package me.ahoo.wow.tck.container
 import org.testcontainers.containers.GenericContainer
 import org.testcontainers.containers.KafkaContainer
 import org.testcontainers.containers.MongoDBContainer
+import org.testcontainers.containers.wait.strategy.Wait
+import org.testcontainers.containers.wait.strategy.WaitAllStrategy
 import org.testcontainers.elasticsearch.ElasticsearchContainer
 import org.testcontainers.utility.DockerImageName
 import java.time.Duration
 
+private class AuthenticatedElasticsearchContainer(
+    dockerImageName: DockerImageName,
+    username: String,
+    password: String,
+    httpPort: Int,
+) : ElasticsearchContainer(dockerImageName) {
+    init {
+        waitingFor(
+            WaitAllStrategy()
+                .withStrategy(super.getWaitStrategy())
+                .withStrategy(
+                    Wait.forHttps("/_security/_authenticate")
+                        .forPort(httpPort)
+                        .allowInsecure()
+                        .withBasicCredentials(username, password)
+                        .forStatusCode(200),
+                ),
+        )
+    }
+}
+
 object WowTestContainers {
     const val ELASTIC_USER = "elastic"
 
-    private const val ELASTIC_PASSWORD = "wow"
+    private const val ELASTIC_PASSWORD = "wow-test"
+    private const val ELASTICSEARCH_HTTP_PORT = 9200
     private const val ELASTICSEARCH_JAVA_OPTS = "-Xms512m -Xmx512m"
+    private val ELASTICSEARCH_STARTUP_TIMEOUT = Duration.ofMinutes(5)
 
     val mongo: MongoDBContainer by lazy {
         MongoDBContainer(DockerImageName.parse(ContainerImages.MONGO))
@@ -40,15 +65,18 @@ object WowTestContainers {
     }
 
     val elasticsearch: ElasticsearchContainer by lazy {
-        ElasticsearchContainer(
+        AuthenticatedElasticsearchContainer(
             DockerImageName
                 .parse(ContainerImages.ELASTICSEARCH_REPOSITORY)
                 .withTag(ContainerImages.ELASTICSEARCH_TAG),
+            username = ELASTIC_USER,
+            password = ELASTIC_PASSWORD,
+            httpPort = ELASTICSEARCH_HTTP_PORT,
         )
             .withPassword(ELASTIC_PASSWORD)
             .withEnv("ES_JAVA_OPTS", ELASTICSEARCH_JAVA_OPTS)
             .withNetworkAliases("elasticsearch")
-            .withStartupTimeout(Duration.ofMinutes(5))
+            .withStartupTimeout(ELASTICSEARCH_STARTUP_TIMEOUT)
             .also { it.start() }
     }
 
