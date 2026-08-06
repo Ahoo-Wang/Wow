0191063f51c4fdd61cdc75de6d8c705e57616b26
Ahoo Wang <ahoowang@qq.com>
2026-08-06 22:45:24 +0800

    test: cover direct query handler entrypoint

--- wow-query/src/test/kotlin/me/ahoo/wow/query/snapshot/filter/DefaultSnapshotQueryHandlerTest.kt
+++ wow-query/src/test/kotlin/me/ahoo/wow/query/snapshot/filter/DefaultSnapshotQueryHandlerTest.kt
@@ -17,7 +17,9 @@ import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
 import me.ahoo.test.asserts.assert
+import me.ahoo.wow.api.modeling.NamedAggregate
 import me.ahoo.wow.api.query.Condition
+import me.ahoo.wow.filter.ErrorAccessor
 import me.ahoo.wow.filter.ErrorHandler
 import me.ahoo.wow.filter.FilterChain
 import me.ahoo.wow.filter.FilterChainBuilder
@@ -26,6 +28,7 @@ import me.ahoo.wow.query.dsl.condition
 import me.ahoo.wow.query.dsl.listQuery
 import me.ahoo.wow.query.dsl.singleQuery
 import me.ahoo.wow.query.filter.QueryContext
+import me.ahoo.wow.query.filter.QueryType
 import me.ahoo.wow.query.snapshot.NoOpSnapshotQueryServiceFactory
 import me.ahoo.wow.query.snapshot.SnapshotQueryService
 import me.ahoo.wow.query.snapshot.SnapshotQueryServiceFactory
@@ -34,6 +37,7 @@ import org.junit.jupiter.api.Test
 import reactor.core.publisher.Mono
 import reactor.kotlin.test.test
 import reactor.test.publisher.PublisherProbe
+import java.util.concurrent.ConcurrentHashMap
 
 class DefaultSnapshotQueryHandlerTest {
     private val tailSnapshotQueryFilter = TailSnapshotQueryFilter<Any>(NoOpSnapshotQueryServiceFactory)
@@ -172,6 +176,46 @@ class DefaultSnapshotQueryHandlerTest {
     }
 
     @Test
+    fun `should delegate direct handle context`() {
+        val context = ErrorQueryContext(MOCK_AGGREGATE_METADATA)
+        val handledContexts = mutableListOf<QueryContext<*, *>>()
+        val chain = FilterChain<QueryContext<*, *>> {
+            handledContexts.add(it)
+            Mono.empty()
+        }
+        val handler = DefaultSnapshotQueryHandler(chain)
+
+        handler.handle(context)
+            .test()
+            .verifyComplete()
+
+        handledContexts.assert().containsExactly(context)
+    }
+
+    @Test
+    fun `should fail closed and store error for direct handle context`() {
+        val failure = IllegalStateException("filter failed")
+        val context = ErrorQueryContext(MOCK_AGGREGATE_METADATA)
+        val chain = FilterChain<QueryContext<*, *>> {
+            throw failure
+        }
+        val resumeErrorHandler = mockk<ErrorHandler<QueryContext<*, *>>> {
+            every { handle(context, failure) } returns Mono.empty()
+        }
+        val handler = DefaultSnapshotQueryHandler(chain, resumeErrorHandler)
+
+        handler.handle(context)
+            .test()
+            .expectErrorSatisfies {
+                it.assert().isSameAs(failure)
+            }
+            .verify()
+
+        context.getError().assert().isSameAs(failure)
+        verify(exactly = 1) { resumeErrorHandler.handle(context, failure) }
+    }
+
+    @Test
     fun `should not execute result after filter chain error`() {
         val failure = IllegalStateException("masking failed")
         val condition = condition { id("1") }
@@ -245,5 +289,23 @@ class DefaultSnapshotQueryHandlerTest {
         capturedConditions.assert().hasSize(2)
         capturedConditions[0].assert().isEqualTo(Condition.and(original, mandatory))
         capturedConditions[1].assert().isEqualTo(capturedConditions[0])
+    }
+}
+
+private class ErrorQueryContext(
+    override val namedAggregate: NamedAggregate,
+) : QueryContext<Condition, Mono<Long>>, ErrorAccessor {
+    override val queryType: QueryType = QueryType.COUNT
+    override val attributes: MutableMap<String, Any> = ConcurrentHashMap()
+    private var error: Throwable? = null
+
+    override fun setError(throwable: Throwable) {
+        error = throwable
+    }
+
+    override fun getError(): Throwable? = error
+
+    override fun clearError() {
+        error = null
     }
 }
