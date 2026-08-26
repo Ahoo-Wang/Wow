8b38cd9af54e078a038797e92bf9ec20e1893315
Ahoo Wang <ahoowang@qq.com>
2026-08-26 12:33:38 +0800

    feat(query): negotiate logical query schemas

---
+++ wow-query/src/main/kotlin/me/ahoo/wow/query/schema/QuerySchemaMerger.kt
@@ -0,0 +1,106 @@
+/*
+ * Copyright [2021-present] [ahoo wang <ahoowang@qq.com> (https://github.com/Ahoo-Wang)].
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package me.ahoo.wow.query.schema
+
+import me.ahoo.wow.api.query.LogicalField
+import me.ahoo.wow.api.query.schema.QueryCardinality
+
+internal class QuerySchemaMerger {
+    fun merge(
+        system: QuerySchemaDeclaration,
+        extensions: List<PrioritizedQuerySchemaDeclaration>,
+    ): LogicalQuerySchema {
+        extensions.forEach { extension ->
+            extension.declaration.fields.forEach { (field, declaration) ->
+                validateExtensionPath(field)
+                system.fields[field]?.rejectSystemOverwrite(field, declaration)
+            }
+        }
+
+        val merged = system.fields.toMutableMap()
+        extensions.groupBy(PrioritizedQuerySchemaDeclaration::priority)
+            .toSortedMap()
+            .forEach { (_, declarations) ->
+                val priorityFields = mutableMapOf<LogicalField, QueryFieldDeclaration>()
+                declarations.forEach { prioritized ->
+                    prioritized.declaration.fields.forEach { (field, declaration) ->
+                        priorityFields[field] = priorityFields[field]
+                            ?.merge(declaration, field, rejectDifferent = true)
+                            ?: declaration
+                    }
+                }
+                priorityFields.forEach { (field, declaration) ->
+                    merged[field] = merged[field]
+                        ?.merge(declaration, field, rejectDifferent = false)
+                        ?: declaration
+                }
+            }
+
+        return LogicalQuerySchema(
+            merged.toSortedMap(compareBy(LogicalField::value)).mapValues { (_, declaration) ->
+                declaration.materialize()
+            },
+        )
+    }
+
+    private fun validateExtensionPath(field: LogicalField) {
+        if (field.value != "state" && !field.value.startsWith("state.")) {
+            throw QuerySchemaValidationException("Snapshot query schema extension must be under [state]: [$field].")
+        }
+    }
+
+    private fun QueryFieldDeclaration.rejectSystemOverwrite(
+        field: LogicalField,
+        extension: QueryFieldDeclaration,
+    ) {
+        rejectSystemLeaf(field, "title", title, extension.title)
+        rejectSystemLeaf(field, "description", description, extension.description)
+        rejectSystemLeaf(field, "enumValues", enumValues, extension.enumValues)
+        rejectSystemLeaf(field, "valueTypes", valueTypes, extension.valueTypes)
+        rejectSystemLeaf(field, "nullable", nullable, extension.nullable)
+        rejectSystemLeaf(field, "required", required, extension.required)
+        rejectSystemLeaf(field, "cardinality", cardinality, extension.cardinality)
+        rejectSystemLeaf(field, "semanticType", semanticType, extension.semanticType)
+        rejectSystemLeaf(field, "dynamicChildren", dynamicChildren, extension.dynamicChildren)
+    }
+
+    private fun rejectSystemLeaf(
+        field: LogicalField,
+        leaf: String,
+        system: DeclarationValue<*>,
+        extension: DeclarationValue<*>,
+    ) {
+        if (system is DeclarationValue.Set && extension is DeclarationValue.Set) {
+            throw QuerySchemaConflictException("System query schema leaf cannot be overwritten: [$field.$leaf].")
+        }
+    }
+
+    private fun QueryFieldDeclaration.materialize() = LogicalQueryFieldSchema(
+        title = title.valueOr(null),
+        description = description.valueOr(null),
+        enumValues = enumValues.valueOr(null),
+        valueTypes = valueTypes.valueOr(emptySet()),
+        nullable = nullable.valueOr(true),
+        required = required.valueOr(false),
+        cardinality = cardinality.valueOr(QueryCardinality.SINGLE),
+        semanticType = semanticType.valueOr(null),
+        dynamicChildren = dynamicChildren.valueOr(false),
+    )
+
+    private fun <T> DeclarationValue<T>.valueOr(default: T): T =
+        when (this) {
+            is DeclarationValue.Set -> value
+            DeclarationValue.Unset -> default
+        }
+}
