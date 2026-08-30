b0b6a9140a381e0a96ba149f6bac37c9193e3452
Ahoo Wang <ahoowang@qq.com>
2026-08-30 22:10:29 +0800

    refactor(wow): remove query schema metadata

--- typescript/wow-client/src/query/index.ts
+++ typescript/wow-client/src/query/index.ts
@@ -25,6 +25,5 @@ export * from './event';
 export * from './snapshot';
 export * from './state';
 export * from './cursorQuery';
-export * from './querySchema';
 export * from './queryClients';
 export * from './types';
