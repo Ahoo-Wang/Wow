e4357a6dffc851c24daca965926da6680d2d846e
Ahoo Wang <ahoowang@qq.com>
2026-09-03 09:15:48 +0800

    refactor(mongo): separate query backend schema provider

--- wow-mongo/src/main/kotlin/me/ahoo/wow/mongo/query/event/MongoEventStreamQueryBackend.kt
+++ wow-mongo/src/main/kotlin/me/ahoo/wow/mongo/query/event/MongoEventStreamQueryBackend.kt
@@ -15,8 +15,6 @@ package me.ahoo.wow.mongo.query.event
 
 import com.mongodb.reactivestreams.client.MongoCollection
 import me.ahoo.wow.api.modeling.NamedAggregate
-import me.ahoo.wow.api.query.schema.QueryModel
-import me.ahoo.wow.modeling.materialize
 import me.ahoo.wow.mongo.Documents.replacePrimaryKeyToId
 import me.ahoo.wow.mongo.query.AbstractMongoFilterConverter
 import me.ahoo.wow.mongo.query.AbstractMongoQueryBackend
@@ -24,57 +22,17 @@ import me.ahoo.wow.mongo.query.MongoProjectionConverter
 import me.ahoo.wow.mongo.query.MongoSortConverter
 import me.ahoo.wow.mongo.toObjectNode
 import me.ahoo.wow.query.event.EventStreamQueryBackend
-import me.ahoo.wow.query.schema.DefaultQueryModelSchemaProvider
-import me.ahoo.wow.query.schema.QueryModelSchemaProvider
-import me.ahoo.wow.query.schema.QuerySchemaContext
-import me.ahoo.wow.query.schema.QuerySchemaUnavailableException
 import org.bson.Document
-import reactor.core.publisher.Mono
 import tools.jackson.databind.node.ObjectNode
 
 class MongoEventStreamQueryBackend(
     override val namedAggregate: NamedAggregate,
     override val collection: MongoCollection<Document>,
     override val converter: AbstractMongoFilterConverter = EventStreamFilterConverter,
-    private val schemaProvider: QueryModelSchemaProvider =
-        defaultSchemaProvider(namedAggregate, collection, converter),
 ) : AbstractMongoQueryBackend(),
-    EventStreamQueryBackend,
-    QueryModelSchemaProvider by schemaProvider {
+    EventStreamQueryBackend {
     override val projectionConverter: MongoProjectionConverter = MongoProjectionConverter(EventStreamFieldConverter)
     override val sortConverter: MongoSortConverter = MongoSortConverter(EventStreamFieldConverter)
     override fun toObjectNode(document: Document): ObjectNode =
         document.replacePrimaryKeyToId().toObjectNode()
-
-    companion object {
-        private fun defaultSchemaProvider(
-            namedAggregate: NamedAggregate,
-            collection: MongoCollection<Document>,
-            converter: AbstractMongoFilterConverter,
-        ): QueryModelSchemaProvider {
-            if (converter !== EventStreamFilterConverter) {
-                return object : QueryModelSchemaProvider {
-                    override fun schema(): Mono<me.ahoo.wow.query.schema.QueryModelSchema> = unavailable()
-
-                    override fun refresh(): Mono<me.ahoo.wow.query.schema.QueryModelSchema> = unavailable()
-
-                    private fun unavailable(): Mono<me.ahoo.wow.query.schema.QueryModelSchema> = Mono.error(
-                        QuerySchemaUnavailableException(
-                            "MongoDB query schema is unavailable for custom filter converters.",
-                        ),
-                    )
-                }
-            }
-            return DefaultQueryModelSchemaProvider(
-                context = QuerySchemaContext(namedAggregate.materialize(), QueryModel.EVENT_STREAM),
-                sources = emptyList(),
-                adapter = me.ahoo.wow.mongo.query.schema.MongoQuerySchemaAdapter(
-                    collection,
-                    null,
-                    QueryModel.EVENT_STREAM,
-                    EventStreamFieldConverter,
-                ),
-            )
-        }
-    }
 }
